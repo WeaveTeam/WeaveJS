efd1e39c217ca1e445cd306f02c2751ef57de5da
franckamayou <franckamayou@gmail.com>
2015-07-08 16:01:52 -0400

    added axes titles and resize callbacks

--- src/weave-c3-scatterplot.js
+++ src/weave-c3-scatterplot.js
@@ -3,61 +3,88 @@ import c3 from "c3";
 import WeavePanel from "./WeavePanel";
 import jquery from "jquery";
 
-// function _buildC3Visualization (weaveState) {
-//     this.c3Opts = {};
-// }
+
+var _plotterPath;
+var _xAxisPath;
+var _yAxisPath;
+var _dataXPath;
+var _dataYPath;
+
+var _c3Options = {
+    data: {
+        size: {},
+        json: {},
+        keys: {
+            x: "x",
+            value: ["y"]
+        },
+        type: "scatter"
+    },
+    axis: {
+        x: {
+            // needs to change
+            label: "",
+            tick: {
+                fit: false
+            }
+        },
+        y: {
+            label: ""
+        }
+    }
+};
+
+function _axisLabelChanged () {
+    var xAxisTitle = _xAxisPath.push("overrideAxisName").getState() || _dataXPath.getValue("ColumnUtils.getTitle(this)");
+    var yAxisTitle = _yAxisPath.push("overrideAxisName").getState() || _dataYPath.getValue("ColumnUtils.getTitle(this)");
+    _c3Options.axis.x.label = xAxisTitle;
+    _c3Options.axis.y.label = yAxisTitle;
+    this.update();
+}
+
+function _dataChanged() {
+    _c3Options.data.json = _plotterPath.retrieveRecords({ x: _dataXPath, y: _dataYPath});
+    this.update();
+}
+
+function _sizeChanged() {
+    _c3Options.size = {
+            height: jquery(this.element).height(),
+            width: jquery(this.element).width()
+    };
+    this.update();
+}
 
 export default class extends WeavePanel {
 
-    constructor(element, toolPath) {
-        super(element, toolPath);
-        this.toolPath = toolPath;
-        this.element = element;
+    constructor(parent, toolPath) {
+        super(parent, toolPath);
+
+        _plotterPath = this.toolPath.pushPlotter("plot");
+
+        _dataXPath = _plotterPath.push("dataX");
+        _dataYPath = _plotterPath.push("dataY");
+
+        _xAxisPath = this.toolPath.pushPlotter("xAxis");
+        _yAxisPath = this.toolPath.pushPlotter("yAxis");
+
+        [_dataXPath, _dataYPath].forEach( (item) => {
+            item.addCallback(_dataChanged.bind(this), true, false);
+        });
+
+        [_dataXPath, _dataYPath, _xAxisPath.push("overrideAxisName"), _yAxisPath.push("overrideAxisName")].forEach((item) => {
+            item.addCallback(_axisLabelChanged.bind(this), true, false);
+        });
+
+        _c3Options.bindto = this.element[0];
+        this.update();
     }
 
-    // This function takes in a weave session state
-    // and creates a c3 visualization object
-    //
-    // This function should not be called externally
-    _buildC3Visualization() {
-
-        var plotterPath = this.toolPath.pushPlotter();
-        var dataXPath = plotterPath.push("dataX");
-        var dataYPath = plotterPath.push("dataY");
-
-        this._c3Opts = {
-             size: {
-                width: jquery(this.div).width(),
-                height: jquery(this.div).height()
-            },
-            data: {
-                json: plotterPath.retrieveRecords({ x: dataXPath, y: dataYPath}),
-                keys: {
-                    x: "x",
-                    value: ["y"]
-                },
-                type: "scatter"
-            },
-            axis: {
-                x: {
-                    // needs to change
-                    label: dataXPath.push(null).getState().metadata.title,
-                    tick: {
-                        fit: false
-                    }
-                },
-                y: {
-                    label: dataYPath.push(null).getState().metadata.title
-                }
-            }
-        };
+    _updateContents () {
+        _sizeChanged.call(this);
     }
 
-    // This function takes a DOM element ID as a target
-    // for generating a c3 visualization.
-    generate() {
-        this.chart = this._buildC3Visualization();
-        this._c3Opts.bindto = this.div[0];
-        this.chart = c3.generate(this._c3Opts);
+    update() {
+        this.chart = c3.generate(_c3Options);
     }
 }
