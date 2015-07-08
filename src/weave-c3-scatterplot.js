06dcb3c98beba6376c711be3187aa730b2d7091a
franckamayou <franckamayou@gmail.com>
2015-07-08 17:57:55 -0400

    Create axis class in scatter plot

--- src/weave-c3-scatterplot.js
+++ src/weave-c3-scatterplot.js
@@ -9,49 +9,31 @@ var _yAxisPath;
 var _dataXPath;
 var _dataYPath;
 
-var _c3Options = {
-    data: {
-        size: {},
-        json: {},
-        keys: {
-            x: "x",
-            value: ["y"]
-        },
-        type: "scatter"
-    },
-    axis: {
-        x: {
-            // needs to change
-            label: "",
-            tick: {
-                fit: false
-            }
-        },
-        y: {
-            label: ""
-        }
+var _xAxis;
+var _yAxis;
+
+class SimpleAxis {
+
+    // This class takes a weavePath to an SimpleAxis plotter
+    constructor(axisPath) {
+
+        this._c3Axis = {};
+        this._axisPath = axisPath;
+        axisPath.addCallback(function() {
+            this._c3Axis.min = this._axisPath.push("axisLineMinValue").getState();
+            this._c3Axis.max = this._axisPath.push("axisLineMaxValue").getState();
+            this._c3Axis.culling = this._axisPath.push("tickCountRequested").getState();
+            this._c3Axis.label = {
+                text: "", // the axis label needs to be set externally
+                position: "outer-center" // weave axisname currently has only one position.
+            };
+        }.bind(this), true, false);
     }
-};
 
-function _axisLabelChanged () {
-    var xAxisTitle = _xAxisPath.push("overrideAxisName").getState() || _dataXPath.getValue("ColumnUtils.getTitle(this)");
-    var yAxisTitle = _yAxisPath.push("overrideAxisName").getState() || _dataYPath.getValue("ColumnUtils.getTitle(this)");
-    _c3Options.axis.x.label = xAxisTitle;
-    _c3Options.axis.y.label = yAxisTitle;
-    this.update();
-}
-
-function _dataChanged() {
-    _c3Options.data.json = _plotterPath.retrieveRecords({ x: _dataXPath, y: _dataYPath});
-    this.update();
-}
-
-function _sizeChanged() {
-    _c3Options.size = {
-            height: jquery(this.element).height(),
-            width: jquery(this.element).width()
-    };
-    this.update();
+    // This function return a C3 Object configuation for an axis
+    getC3Axis () {
+        return this._c3Axis;
+    }
 }
 
 export default class extends WeavePanel {
@@ -59,6 +41,18 @@ export default class extends WeavePanel {
     constructor(parent, toolPath) {
         super(parent, toolPath);
 
+        this._c3Options = {
+            data: {
+                size: {},
+                json: [],
+                keys: {
+                    x: "x",
+                    value: ["y"]
+                },
+                type: "scatter"
+            },
+            axis: {}
+        };
         _plotterPath = this.toolPath.pushPlotter("plot");
 
         _dataXPath = _plotterPath.push("dataX");
@@ -67,23 +61,53 @@ export default class extends WeavePanel {
         _xAxisPath = this.toolPath.pushPlotter("xAxis");
         _yAxisPath = this.toolPath.pushPlotter("yAxis");
 
+        _xAxis = new SimpleAxis(_xAxisPath);
+        _yAxis = new SimpleAxis(_yAxisPath);
+
         [_dataXPath, _dataYPath].forEach( (item) => {
-            item.addCallback(_dataChanged.bind(this), true, false);
+            item.addCallback(this._dataChanged.bind(this), true, false);
         });
 
         [_dataXPath, _dataYPath, _xAxisPath.push("overrideAxisName"), _yAxisPath.push("overrideAxisName")].forEach((item) => {
-            item.addCallback(_axisLabelChanged.bind(this), true, false);
+            item.addCallback(this._axisChanged.bind(this), true, false);
         });
 
-        _c3Options.bindto = this.element[0];
+        this._c3Options.bindto = this.element[0];
         this.update();
     }
 
     _updateContents () {
-        _sizeChanged.call(this);
+        this._sizeChanged.call(this);
+    }
+
+    _axisChanged () {
+
+        this._c3Options.axis.x = _xAxis.getC3Axis();
+        this._c3Options.axis.x.label.text = _xAxisPath.push("overrideAxisName").getState() || _dataXPath.getValue("ColumnUtils.getTitle(this)");
+
+        this._c3Options.axis.y = _yAxis.getC3Axis();
+        this._c3Options.axis.y.label.text = _xAxisPath.push("overrideAxisName").getState() || _dataXPath.getValue("ColumnUtils.getTitle(this)");
+
+        this.update();
+    }
+
+    _dataChanged() {
+        this._c3Options.data.json = _plotterPath.retrieveRecords({ x: _dataXPath, y: _dataYPath});
+        this.update();
+    }
+
+    _sizeChanged() {
+        this._c3Options.size = {
+                height: jquery(this.element).height(),
+                width: jquery(this.element).width()
+        };
+        this.update();
     }
 
     update() {
-        this.chart = c3.generate(_c3Options);
+        console.log(this._c3Options);
+        this.chart = c3.generate(this._c3Options);
     }
 }
+
+
