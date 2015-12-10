af50b2225e47a176bc3e7cee7d45440d6da7b578
Andy Dufilie <andrew.dufilie@iweave.com>
2015-12-09 23:33:38 -0500

    added code to load WeaveJS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 /* eslint-disable */
 import WeaveLayoutManager from "./WeaveLayoutManager.jsx";
+import Weave from "./weave-shim.js";
 /* eslint-enable */
 
+var weave = opener && opener.weave;
+if (!weave)
+{
+	weave = new Weave();
+	weave.path().state(WeaveTest.getIrisState());
+}
 window.wlm = React.render(
-		<WeaveLayoutManager weave={opener ? opener.weave : null}/>,	document.body
+		<WeaveLayoutManager weave={weave}/>,	document.body
 );
