dbc6e6b26680e90581761273241462c5997e4989
Andy Dufilie <andrew.dufilie@iweave.com>
2015-12-29 19:23:32 -0500

    fixed missing JSZip problem

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import $ from "jquery";
 import StandardLib from "../outts/Utils/StandardLib";
+import JSZip from "jszip";
 /* eslint-disable */
 import WeaveLayoutManager from "./WeaveLayoutManager.jsx";
 /* eslint-enable */
 
 /*global Weave, weavejs*/
+
+// temporary solution
+weavejs.util.JS.JSZip = JSZip;
 
 var weave = window.weave || (opener && opener.weave);
 
@@ -14,7 +18,7 @@ var urlParams = StandardLib.getUrlParams();
 if (!weave && urlParams.file)
 {
 	weave = new Weave();
-	weavejs.core.WeaveArchive.loadUrl(weave, urlParams.file).then(render);
+	weavejs.core.WeaveArchive.loadUrl(weave, urlParams.file).then(render, e => { console.error(e) });
 }
 else
 {
