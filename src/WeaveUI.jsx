6f1222291e7ec80471d37fe8ccaea19b8c140888
franckamayou <franckamayou@gmail.com>
2016-01-06 12:19:05 -0500

    added weaveui exported file

---
+++ src/WeaveUI.jsx
@@ -0,0 +1,23 @@
+import WeaveLayoutManager from "./WeaveLayoutManager";
+import WeaveC3Barchart from "../outts/tools/weave-c3-barchart.jsx";
+import WeaveC3ScatterPlot from "../outts/tools/weave-c3-scatterplot.jsx";
+import WeaveC3ColorLegend from "../outts/tools/weave-c3-colorlegend.jsx";
+import WeaveC3LineChart from "./tools/weave-c3-linechart.jsx";
+import WeaveC3PieChart from "../outts/tools/weave-c3-piechart.jsx";
+import WeaveC3Histogram from "../outts/tools/weave-c3-histogram.jsx";
+import WeaveOpenLayersMap from "./tools/map.js";
+import WeaveReactTable from "./tools/weave-react-table-2.jsx";
+import SessionStateMenuTool from "./tools/weave-session-state-menu.jsx";
+
+export default {
+    WeaveLayoutManager,
+    WeaveC3Barchart,
+    WeaveC3ScatterPlot,
+    WeaveC3ColorLegend,
+    WeaveC3LineChart,
+    WeaveC3PieChart,
+    WeaveC3Histogram,
+    WeaveOpenLayersMap,
+    WeaveReactTable,
+    SessionStateMenuTool
+}
