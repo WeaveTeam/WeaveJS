66e2b677f3bdb320c740afe29c633cfd43942894
Zachary Maybury <maybury@iweave.com>
2016-01-13 14:40:24 -0500

    Removal of old JSX tools and updated references to new Typescript replacements

--- src/WeaveUI.jsx
+++ src/WeaveUI.jsx
@@ -2,12 +2,12 @@ import WeaveLayoutManager from "./WeaveLayoutManager";
 import WeaveC3Barchart from "../outts/tools/weave-c3-barchart.jsx";
 import WeaveC3ScatterPlot from "../outts/tools/weave-c3-scatterplot.jsx";
 import WeaveC3ColorLegend from "../outts/tools/weave-c3-colorlegend.jsx";
-import WeaveC3LineChart from "./tools/weave-c3-linechart.jsx";
+import WeaveC3LineChart from "../outts/tools/weave-c3-linechart.jsx";
 import WeaveC3PieChart from "../outts/tools/weave-c3-piechart.jsx";
 import WeaveC3Histogram from "../outts/tools/weave-c3-histogram.jsx";
 import WeaveOpenLayersMap from "./tools/map.js";
 import WeaveReactTable from "./tools/weave-react-table.jsx";
-import SessionStateMenuTool from "./tools/weave-session-state-menu.jsx";
+import SessionStateMenuTool from "../outts/tools/weave-session-state-menu.jsx";
 import StandardLib from "../outts/utils/StandardLib.js";
 import JSZip from "jszip";
 
