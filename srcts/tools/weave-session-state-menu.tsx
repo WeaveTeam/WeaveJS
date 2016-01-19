832d5f43ae384c5a5bb692d24be71a70ce96f5e2
Zachary Maybury <maybury@iweave.com>
2016-01-19 12:58:05 -0500

    Implementation of ComboBox option for Session State Menu Tool #WEAVE-42 Fixed Spent time 2h

--- srcts/tools/weave-session-state-menu.tsx
+++ srcts/tools/weave-session-state-menu.tsx
@@ -7,7 +7,8 @@
 import AbstractWeaveTool from "./AbstractWeaveTool";
 import {registerToolImplementation} from "../WeaveTool";
 import * as React from "react";
-import {ListGroupItem, ListGroup} from "react-bootstrap";
+import ui from "../react-ui/ui";
+import {ListGroupItem, ListGroup, DropdownButton, MenuItem} from "react-bootstrap";
 import * as _ from "lodash";
 import {IAbstractWeaveToolProps} from "./AbstractWeaveTool";
 import {IAbstractWeaveToolPaths} from "./AbstractWeaveTool";
@@ -17,6 +18,7 @@ import {CSSProperties} from "react";
 //TODO: This is a hack to allow react to be imported in generated JSX. Without this, import is missing and render encounters an exception
 var stub:any = React;
 const sessionStateMenuStyle:CSSProperties = {display:"flex", flex:1, height:"100%", flexDirection:"column", overflow:"auto"};
+const sessionStateComboBoxStyle:CSSProperties = {display:"flex", flex:1, height:"100%", flexDirection:"column", overflow:"auto"};
 
 class SessionStateMenuTool extends AbstractWeaveTool {
     private choices:WeavePath;
@@ -26,6 +28,7 @@ class SessionStateMenuTool extends AbstractWeaveTool {
         super(props);
         this.toolPath.push("choices").addCallback(this, this.forceUpdate);
         this.toolPath.push("selectedChoice").addCallback(this, this.forceUpdate);
+        this.toolPath.push("layoutMode").addCallback(this, this.forceUpdate);
     }
 
     protected handleMissingSessionStateProperties(newState:any)
@@ -48,18 +51,36 @@ class SessionStateMenuTool extends AbstractWeaveTool {
     render() {
         this.choices = this.toolPath.push("choices");
         var selectedChoice:string = this.toolPath.getState("selectedChoice");
+        var layoutMode:string = this.toolPath.getState("layoutMode");
 
         var menus:JSX.Element[] = this.choices.getNames().map((choice:string, index:number) => {
-            return choice === selectedChoice ? <ListGroupItem active key={index} onClick={this.handleItemClick.bind(this, index)}>{choice}</ListGroupItem>
-                : <ListGroupItem key={index} onClick={this.handleItemClick.bind(this, index)}>{choice}</ListGroupItem>;
+            if(layoutMode === "ComboBox"){
+                return choice === selectedChoice ?<MenuItem active key={index} onSelect={this.handleItemClick.bind(this, index)}>{choice}</MenuItem>
+                    :<MenuItem key={index} onSelect={this.handleItemClick.bind(this, index)}>{choice}</MenuItem>;
+            }else {
+                return choice === selectedChoice ?<ListGroupItem active key={index} onClick={this.handleItemClick.bind(this, index)}>{choice}</ListGroupItem>
+                    :<ListGroupItem key={index} onClick={this.handleItemClick.bind(this, index)}>{choice}</ListGroupItem>;
+            }
         });
 
-        return (<div style={sessionStateMenuStyle}>
-            <ListGroup>
-                {
-                    menus
-                }
-            </ListGroup>
+        var container:JSX.Element;
+
+        if(layoutMode === "ComboBox"){
+            container =
+                <ui.VBox style={{height:"100%", flex:1.0, alignItems:"center"}}>
+                    <DropdownButton title={selectedChoice} id={`dropdown-${this.toolPath.getState("class")}`} >
+                        {menus}
+                    </DropdownButton>
+                </ui.VBox>
+        }else{
+            container =
+                <ListGroup>
+                    {menus}
+                </ListGroup>
+        }
+
+        return (<div style={layoutMode === "ComboBox" ? sessionStateComboBoxStyle : sessionStateMenuStyle}>
+            {container}
         </div>);
     }
 }
