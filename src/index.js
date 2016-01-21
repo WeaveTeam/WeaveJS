3d010cc1a6c5fc8ec57dbbc49b6bd29bc9e62eef
Andy Dufilie <andrew.dufilie@iweave.com>
2016-01-21 14:40:58 -0500

    When opened from Flash, loading everything from a WeaveArchive generated by Flash.

--- src/index.js
+++ src/index.js
@@ -10,26 +10,39 @@ import WeaveLayoutManager from "../outts/WeaveLayoutManager.jsx";
 
 /*global Weave, weavejs*/
 
-// temporary solution
+// TEMPORARY SOLUTION
 weavejs.util.JS.JSZip = JSZip;
 
 var weave = window.weave;
-
-var urlParams = StandardLib.getUrlParams();
-if (!weave && urlParams.file) {
-    window.weave = weave = new Weave();
-    weavejs.core.WeaveArchive.loadUrl(weave, urlParams.file).then(render);
+if (weave)
+{
+	render();
 }
-else {
-    render();
+else
+{
+	window.weave = weave = new Weave();
+	var urlParams = StandardLib.getUrlParams();
+	var weaveExternalTools = window.opener && window.opener.WeaveExternalTools;
+	if (urlParams.file)
+	{
+		weavejs.core.WeaveArchive.loadUrl(weave, urlParams.file).then(render);
+	}
+	else if (weaveExternalTools && weaveExternalTools[window.name])
+	{
+		// read content from flash
+		var ownerPath = weaveExternalTools[window.name].path;
+		var content = atob(ownerPath.getValue('btoa(Weave.createWeaveFileContent())'));
+		weavejs.core.WeaveArchive.loadFileContent(weave, content);
+		render();
+	}
+	else
+	{
+		render();
+	}
 }
 
 function render() {
-    if(weave) {
-        $(() => {
-            ReactDOM.render(<WeaveLayoutManager weave={weave}/>, document.getElementById("weaveElt"));
-        });
-    } else {
-        console.warn("missing weave instance.");
-    }
+	$(() => {
+		ReactDOM.render(<WeaveLayoutManager weave={weave}/>, document.getElementById("weaveElt"));
+	});
 }
