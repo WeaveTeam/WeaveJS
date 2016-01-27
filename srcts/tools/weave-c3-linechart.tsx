ab9a3cfe13ec8302b335b0956de67125bb496e3a
Zachary Maybury <maybury@iweave.com>
2016-01-27 00:32:23 -0500

    Refactors to make C3 Line Chart more efficient and code cleaner, as well as rotate labels when chart is flipped. #WEAVE-70 Fixed Spent time 4h

--- srcts/tools/weave-c3-linechart.tsx
+++ srcts/tools/weave-c3-linechart.tsx
@@ -7,16 +7,17 @@
 import {IVisToolProps} from "./IVisTool";
 import {IToolPaths} from "./AbstractC3Tool";
 import AbstractC3Tool from "./AbstractC3Tool";
-
 import {registerToolImplementation} from "../WeaveTool";
 import * as d3 from "d3";
 import * as _ from "lodash";
 import FormatUtils from "../utils/FormatUtils";
 import * as React from "react";
-import {ChartConfiguration, ChartAPI, generate} from "c3";
+import * as c3 from "c3";
+import {ChartConfiguration, ChartAPI} from "c3";
 import {MouseEvent} from "react";
 import {getTooltipContent} from "./tooltip";
 import Tooltip from "./tooltip";
+import StandardLib from "../utils/StandardLib";
 
 interface ILineChartPaths extends IToolPaths {
     plotter: WeavePath;
@@ -39,17 +40,144 @@ class WeaveC3LineChart extends AbstractC3Tool {
     private records:Record[][];
     private columnLabels:string[];
     private columnNames:string[];
+    private columns:any;
+    private chartType:string;
+
     private flag:boolean;
+    private busy:boolean;
+    private dirty:boolean;
 
     protected chart:ChartAPI;
     protected c3Config:ChartConfiguration;
+    protected c3ConfigYAxis:c3.YAxisConfiguration;
+
     protected paths:ILineChartPaths;
 
     constructor(props:IVisToolProps) {
         super(props);
+        this.busy = false;
         this.keyToIndex = {};
         this.indexToKey = {};
         this.yAxisValueToLabel = {};
+        this.columns = [];
+        this.validate = _.debounce(this.validate.bind(this), 30);
+
+        this.c3Config = {
+            size: {
+                width: this.props.style.width,
+                height: this.props.style.height
+            },
+            padding: {
+                top: 20,
+                bottom: 20
+            },
+            data: {
+                columns: [],
+                xSort: false,
+                selection: {
+                    enabled: true,
+                    multiple: true,
+                    draggable: true
+                },
+                onclick: (d:any) => {
+                    var event:MouseEvent = this.chart.internal.d3.event as MouseEvent;
+                    if(!(event.ctrlKey || event.metaKey) && d && d.hasOwnProperty("index")) {
+                        this.toolPath.selection_keyset.setKeys([d.id]);
+                    }
+                },
+                onselected: (d:any) => {
+                    this.flag = true;
+                    if(d && d.hasOwnProperty("index")) {
+                        this.toolPath.selection_keyset.addKeys([d.id]);
+                    }
+                },
+                onunselected: (d:any) => {
+                    this.flag = true;
+                    if(d && d.hasOwnProperty("index")) {
+                        this.toolPath.selection_keyset.removeKeys([d.id]);
+                    }
+                },
+                onmouseover: (d:any) => {
+                    if(d && d.hasOwnProperty("index")) {
+                        this.toolPath.probe_keyset.setKeys([d.id]);
+                    }
+
+                    var columnNamesToValue:{[columnName:string] : string|number } = {};
+                    this.columnLabels.forEach( (label:string,index:number,array:any[]) => {
+                        columnNamesToValue[label] = this.numericRecords[d.index]["columns"][index] as number;
+                    });
+
+                    this.props.toolTip.setState({
+                        x: this.chart.internal.d3.event.pageX,
+                        y: this.chart.internal.d3.event.pageY,
+                        showToolTip: true,
+                        columnNamesToValue: columnNamesToValue
+                    });
+                },
+                onmouseout: (d:any) => {
+                    if(d && d.hasOwnProperty("index")) {
+                        this.toolPath.probe_keyset.setKeys([]);
+                        this.props.toolTip.setState({
+                            showToolTip: false
+                        });
+                    }
+                }
+            },
+            tooltip: {
+                show: false
+            },
+            grid: {
+                x: {
+                    show: true
+                },
+                y: {
+                    show: true
+                }
+            },
+            axis: {
+                x: {
+                    tick: {
+                        culling: {
+                            max: null
+                        },
+                        multiline: false,
+                        rotate: -45,
+                        format: (d:number):string => {
+                            if(this.c3Config.axis.y2.show){
+                                //handle case where labels need to be reversed
+                                var temp:number = this.columnLabels.length-1;
+                                return this.columnLabels[temp-d];
+                            }else{
+                                return this.columnLabels[d];
+                            }
+                        }
+                    }
+                }
+            },
+            bindto: null,
+            legend: {
+                show: false
+            },
+            onrendered: () => {
+                this.busy = false;
+                this.updateStyle();
+                if (this.dirty)
+                    this.validate();
+            }
+        };
+        this.c3ConfigYAxis = {
+            show: true,
+            tick: {
+                multiline: true,
+                format: (num:number):string => {
+                    if(this.yLabelColumnPath && this.yLabelColumnPath.getValue("this.getMetadata('dataType')") !== "number") {
+                        return this.yAxisValueToLabel[num] || "";
+                    } else {
+                        return String(FormatUtils.defaultNumberFormatting(num));
+                    }
+                }
+            }
+        }
     }
 
     protected handleMissingSessionStateProperties(newState:any)
@@ -136,36 +264,13 @@ class WeaveC3LineChart extends AbstractC3Tool {
         }
     }
 
-    _updateStyle() {
+    private updateStyle() {
         d3.select(this.element).selectAll("circle").style("opacity", 1)
             .style("stroke", "black")
             .style("stroke-opacity", 0.0);
     }
 
-    mirrorVertical() {
-        var temp:any =  {};
-        if(this.c3Config.axis.y.show == true){
-            this.stringRecords.forEach( (record) => {
-                temp[record["id"].toString()] = 'y2';
-            });
-            this.c3Config.axis.y2.show = true;
-            this.c3Config.axis.y.show = false;
-            this.c3Config.data.axes = temp;
-            this.c3Config.axis.y2.label = this.c3Config.axis.y.label;
-        }else{
-            this.stringRecords.forEach( (record) => {
-                temp[record["id"].toString()] = 'y';
-            });
-            this.c3Config.axis.y.show = true;
-            this.c3Config.axis.y2.show = false;
-            this.c3Config.data.axes = temp;
-            this.c3Config.axis.y.label = this.c3Config.axis.y2.label;
-        }
-        this.chart = generate(this.c3Config);
-        this._dataChanged();
-    }
-
-    _dataChanged() {
+    private dataChanged() {
         this.columnLabels = [];
         this.columnNames = [];
 
@@ -220,9 +325,7 @@ class WeaveC3LineChart extends AbstractC3Tool {
             this.yAxisValueToLabel[numericRecord["yLabel"] as number] = record["yLabel"] as string;
         });
 
-        var columns:any = [];
-
-        columns = this.numericRecords.map(function(record:Record) {
+        this.columns = this.numericRecords.map(function(record:Record) {
             var tempArr:any[] = [];
             tempArr.push(record["id"]);
             _.keys(record["columns"]).forEach((key:string) => {
@@ -236,13 +339,13 @@ class WeaveC3LineChart extends AbstractC3Tool {
             this.colors[record["id"] as string] = ((record["line"] as Record)["color"] as string) || "#C0CDD1";
         });
 
-        var chartType:string = "line";
+        this.chartType= "line";
         if(this.paths.plotter.push("curveType").getState() === "double") {
-            chartType = "spline";
+            this.chartType = "spline";
         }
 
-        if(this.c3Config.axis.y2.show){
-            columns.forEach( (column:any[], index:number, array:any) => {
+        if(this.c3ConfigYAxis.show == false){
+            this.columns.forEach( (column:any[], index:number, array:any) => {
                 var temp:any[] = [];
                 temp.push(column.shift());
                 column = column.reverse();
@@ -252,7 +355,6 @@ class WeaveC3LineChart extends AbstractC3Tool {
                 array[index] = temp;
             });
         }
-        this.chart.load({columns: columns, colors: this.colors, type: chartType, unload: true});
     }
 
     componentWillUnmount() {
@@ -263,16 +365,16 @@ class WeaveC3LineChart extends AbstractC3Tool {
 
     componentDidMount() {
         this.element.addEventListener("click", this.handleClick.bind(this));
-        var dataChanged:Function = _.debounce(this._dataChanged.bind(this), 100);
         var selectionKeySetChanged:Function = this._selectionKeysChanged.bind(this);
         var probeKeySetChanged:Function = _.debounce(this._probedKeysChanged.bind(this), 100);
+
         var plotterPath:WeavePath = this.toolPath.pushPlotter("plot");
         var mapping = [
-            { name: "plotter", path: plotterPath, callbacks: null},
-            { name: "columns", path: plotterPath.push("columns"), callbacks: dataChanged },
-            { name: "lineStyle", path: plotterPath.push("lineStyle"), callbacks: dataChanged },
-            { name: "curveType", path: plotterPath.push("curveType"), callbacks: dataChanged },
-            { name: "filteredKeySet", path: plotterPath.push("filteredKeySet"), callbacks: dataChanged },
+            { name: "plotter", path: plotterPath, callbacks: this.validate},
+            { name: "columns", path: plotterPath.push("columns") },
+            { name: "lineStyle", path: plotterPath.push("lineStyle") },
+            { name: "curveType", path: plotterPath.push("curveType") },
+            { name: "filteredKeySet", path: plotterPath.push("filteredKeySet") },
             { name: "selectionKeySet", path: this.toolPath.selection_keyset, callbacks: selectionKeySetChanged},
             { name: "probeKeySet", path: this.toolPath.probe_keyset, callbacks: probeKeySetChanged}
         ];
@@ -281,131 +383,12 @@ class WeaveC3LineChart extends AbstractC3Tool {
 
        	this.paths.filteredKeySet.getObject().setColumnKeySources(this.paths.columns.getObject().getObjects());
 
-        this.c3Config = {
-            size: {
-                width: this.props.style.width,
-                height: this.props.style.height
-            },
-            padding: {
-                top: 20,
-                bottom: 20
-            },
-            data: {
-                columns: [],
-                xSort: false,
-                selection: {
-                    enabled: true,
-                    multiple: true,
-                    draggable: true
-                },
-                onclick: (d:any) => {
-                    var event:MouseEvent = this.chart.internal.d3.event as MouseEvent;
-                    if(!(event.ctrlKey || event.metaKey) && d && d.hasOwnProperty("index")) {
-                        this.toolPath.selection_keyset.setKeys([d.id]);
-                    }
-                },
-                onselected: (d:any) => {
-                    this.flag = true;
-                    if(d && d.hasOwnProperty("index")) {
-                        this.toolPath.selection_keyset.addKeys([d.id]);
-                    }
-                },
-                onunselected: (d:any) => {
-                    this.flag = true;
-                    if(d && d.hasOwnProperty("index")) {
-                        this.toolPath.selection_keyset.removeKeys([d.id]);
-                    }
-                },
-                onmouseover: (d:any) => {
-                    if(d && d.hasOwnProperty("index")) {
-                        this.toolPath.probe_keyset.setKeys([d.id]);
-                    }
+        this.c3Config.bindto = this.element;
+        if(this.paths.columns.getState().length){
+            this.c3Config.axis.x.height = this.props.style.height * 0.2;
+        }
 
-                    var columnNamesToValue:{[columnName:string] : string|number } = {};
-                    this.columnLabels.forEach( (label:string,index:number,array:any[]) => {
-                       columnNamesToValue[label] = this.numericRecords[d.index]["columns"][index] as number;
-                    });
-
-                    this.props.toolTip.setState({
-                        x: this.chart.internal.d3.event.pageX,
-                        y: this.chart.internal.d3.event.pageY,
-                        showToolTip: true,
-                        columnNamesToValue: columnNamesToValue
-                    });
-                },
-                onmouseout: (d:any) => {
-                    if(d && d.hasOwnProperty("index")) {
-                        this.toolPath.probe_keyset.setKeys([]);
-                        this.props.toolTip.setState({
-                            showToolTip: false
-                        });
-                    }
-                }
-            },
-            tooltip: {
-                show: false
-            },
-            grid: {
-                x: {
-                    show: true
-                },
-                y: {
-                    show: true
-                }
-            },
-            axis: {
-                x: {
-                    tick: {
-                        multiline: false,
-                        rotate: 0,
-                        format: (d:number):string => {
-                            if(this.c3Config.axis.y2.show){
-                                //handle case where labels need to be reversed
-                                var temp:number = this.columnLabels.length-1;
-                                return this.columnLabels[temp-d];
-                            }else{
-                                return this.columnLabels[d];
-                            }
-                        }
-                    }
-                },
-                y: {
-                    show: true,
-                    tick: {
-                        multiline: true,
-                        format: (num:number):string => {
-                            if(this.yLabelColumnPath && this.yLabelColumnPath.getValue("this.getMetadata('dataType')") !== "number") {
-                                return this.yAxisValueToLabel[num] || "";
-                            } else {
-                                return String(FormatUtils.defaultNumberFormatting(num));
-                            }
-                        }
-                    }
-                },
-                y2: {
-                    show: false,
-                    min: null,
-                    max: null,
-                    tick: {
-                        multiline: true,
-                        format: (num:number):string => {
-                            if(this.yLabelColumnPath && this.yLabelColumnPath.getObject().getMetadata('dataType') !== "number") {
-                                return this.yAxisValueToLabel[num] || "";
-                            } else {
-                                return String(FormatUtils.defaultNumberFormatting(num));
-                            }
-                        }
-                    }
-                }
-            },
-            bindto: this.element,
-            legend: {
-                show: false
-            },
-            onrendered: this._updateStyle.bind(this)
-        };
-
-        this.chart = generate(this.c3Config);
+        this.validate(true);
     }
 
     handleClick(event:MouseEvent):void {
@@ -414,7 +397,103 @@ class WeaveC3LineChart extends AbstractC3Tool {
         }
         this.flag = false;
     }
+
+    componentDidUpdate() {
+        // TODO avoid calling generate at each componentDidUpdate
+        this.c3Config.size = {width: this.props.style.width, height: this.props.style.height};
+        if(this.paths.columns.getState().length){
+            this.c3Config.axis.x.height = this.props.style.height * 0.2;
+        }else{
+            this.c3Config.axis.x.height = null;
+        }
+        this.validate(true);
+    }
+
+    validate(forced:boolean = false):void
+    {
+        if (this.busy)
+        {
+            this.dirty = true;
+            return;
+        }
+        this.dirty = false;
+
+        var changeDetected:boolean = false;
+        var axisChange:boolean = this.detectChange('columns');
+        if (axisChange || this.detectChange('plotter', 'curveType', 'lineStyle','filteredKeySet'))
+        {
+            changeDetected = true;
+            this.dataChanged();
+        }
+        if (axisChange)
+        {
+            changeDetected = true;
+            var xLabel:string = " ";//this.paths.xAxis.push("overrideAxisName").getState() || this.paths.dataX.getObject().getMetadata('title');
+            var yLabel:string = " ";//this.paths.yAxis.push("overrideAxisName").getState() || this.paths.dataY.getObject().getMetadata('title');
+
+
+            if (this.numericRecords)
+            {
+                var temp:any =  {};
+                if (weavejs.WeaveAPI.Locale.reverseLayout)
+                {
+                    this.stringRecords.forEach( (record) => {
+                        temp[record["id"].toString()] = 'y2';
+                    });
+                    this.c3Config.data.axes = temp;
+                    this.c3Config.axis.y2 = this.c3ConfigYAxis;
+                    this.c3Config.axis.y = {show: false};
+                    this.c3Config.padding.left = 20;
+                    this.c3Config.padding.right = undefined;
+                    if(this.c3Config.axis.x.tick.rotate)
+                        this.c3Config.axis.x.tick.rotate = 45;
+                }
+                else
+                {
+                    this.stringRecords.forEach( (record) => {
+                        temp[record["id"].toString()] = 'y';
+                    });
+                    this.c3Config.data.axes = temp;
+                    this.c3Config.axis.y = this.c3ConfigYAxis;
+                    delete this.c3Config.axis.y2;
+                    this.c3Config.padding.left = undefined;
+                    this.c3Config.padding.right = 20;
+                    if(this.c3Config.axis.x.tick.rotate)
+                        this.c3Config.axis.x.tick.rotate = -45;
+                }
+            }
+
+            // axis label culling requires this.chart.internal.width
+            if (this.chart)
+            {
+                var width:number = this.chart.internal.width;
+                var textHeight:number = StandardLib.getTextHeight("test", "14pt Helvetica Neue");
+                var xLabelsToShow:number = Math.floor(width / textHeight);
+                xLabelsToShow = Math.max(2,xLabelsToShow);
+                this.c3Config.axis.x.tick.culling = {max: xLabelsToShow};
+            }
+
+            this.c3Config.axis.x.label = {text:xLabel, position:"outer-center"};
+            this.c3ConfigYAxis.label = {text:yLabel, position:"outer-middle"};
+
+        }
+        if (changeDetected || forced)
+        {
+            this.busy = true;
+            this.chart = c3.generate(this.c3Config);
+            this.loadData();
+        }
+    }
+
+    loadData() {
+        if(!this.chart || this.busy)
+            return StandardLib.debounce(this, 'loadData');
+        this.chart.load({columns: this.columns, colors: this.colors, type: this.chartType, unload: true});
+    }
 }
+
+
+
 
 export default WeaveC3LineChart;
 
