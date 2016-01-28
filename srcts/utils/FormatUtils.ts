353c836366ff482000c22bb68d1d75e55388e194
Zachary Maybury <maybury@iweave.com>
2016-01-28 13:23:11 -0500

    Added thousands separator to default number formatting #WEAVE-63

--- srcts/utils/FormatUtils.ts
+++ srcts/utils/FormatUtils.ts
@@ -7,11 +7,11 @@ export default class FormatUtils {
   // this function returns the default number formating.
 	// for number values, we round them to at most 4 decimal places
 	// unless the number is very small, in which case we just return it
-  static defaultNumberFormatting(x:number):number {
+  static defaultNumberFormatting(x:number):number|string {
     if(x < 0.0001) {
       return x;
     } else {
-      return d3.round(x, 4);
+      return d3.format(",")(d3.round(x, 4));
     }
   }
 }
