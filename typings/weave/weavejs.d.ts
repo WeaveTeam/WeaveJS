5952d26d4f3650f89a884c9b834e9d2e96f42c0f
Philip "digitalfox" Kovac <philip.kovac@iweave.com>
2016-01-29 13:53:31 -0500

    Added type definitions for IAttributeColumn, IKeySet, ILinkableHashMap

--- typings/weave/weavejs.d.ts
+++ typings/weave/weavejs.d.ts
@@ -11,6 +11,12 @@ declare module weavejs_fake.api.core {
 	class ILinkableObject {
 
 	}
+
+	class ILinkableCompositeObject extends ILinkableObject {
+		getSessionState(): Array<any>;
+		setSessionState(newState: Array<any>, removeMissingDynamicObjects: boolean): void;
+	}
+
 	class ICallbackCollection extends ILinkableObject {
 		addImmediateCallback(relevantContext: any, callback: Function, runCallbackNow?: boolean, alwaysCallLast?: boolean): void;
 		addGroupedCallback(relevantContext: any, groupedCallback: Function, runCallbackNow?: boolean, delayWhileBusy?: boolean): void;
@@ -23,9 +29,44 @@ declare module weavejs_fake.api.core {
 		callbacksAreDelayed: boolean;
 		triggerCounter: number;
 	}
+
+	class ILinkableHashMap extends ILinkableCompositeObject {
+		typeRestriction: Function;
+		childListCallbacks: any;
+
+		setNameOrder(newOrder: Array<string>);
+		getNames(classFilter: Function): Array<string>;
+		getObjects(classFilter: Function): Array<ILinkableObject>;
+		getObject(name: string): ILinkableObject;
+		getName(object: ILinkableObject): string;
+		requestObject(name: string, classDef: Function, lockObject: boolean): any;
+		requestObjectCopy(name: string, objectToCopy: ILinkableObject): ILinkableObject;
+		renameObject(oldName: string, newName: String): ILinkableObject;
+		objectIsLocked(name: string): boolean;
+		removeObject(name: string): void;
+		removeAllObjects(): void;
+		generateUniqueName(baseName: string): string;
+	}
 }
 
-declare class KeySet extends weavejs_fake.api.core.ICallbackCollection {
+declare module weavejs_fake.api.data
+{
+	class IKeyFilter {
+		containsKey(key: IQualifiedKey): boolean;
+	}
+
+	class IKeySet {
+		keys: Array<IQualifiedKey>;
+	}
+
+	class IAttributeColumn extends IKeySet {
+		getMetadata(propertyName: string): string;
+		getMetadataPropertyNames(): Array<string>;
+		getValueFromKey(key: IQualifiedKey, dataType?: Function);
+	}
+}
+
+declare class KeySet extends weavejs_fake.api.core.ICallbackCollection implements weavejs_fake.api.data.IKeySet {
 	keys: Array<IQualifiedKey>;
 	replaceKeys(keys: Array<IQualifiedKey>): boolean;
 	clearKeys(): boolean;
