2db39e17a93e4cab1ae7f7b14e5d2eb8676ecc5c
Andy Dufilie <andrew.dufilie@iweave.com>
2016-03-17 21:41:53 -0400

    avoid creating a new dom element each time we call prefixer()

--- srcts/react-ui/VendorPrefixer.ts
+++ srcts/react-ui/VendorPrefixer.ts
@@ -4,36 +4,37 @@ var dom_prefixes:string[] = ["Moz", "webKit", "ms"];
 
 var vendorSpecificProperties:string[] = [];
 var vendor:string;
+var styleKeys:string[];
 
-function getVendorSpecificProperties():string[] {
-	if(vendorSpecificProperties.length)
+function getVendorSpecificProperties():string[]
+{
+	if (vendorSpecificProperties.length)
 		return vendorSpecificProperties;
-	else
+	
+	if (!styleKeys)
+		styleKeys = Object.keys(document.createElement("div").style);
+	
+	for (var property of styleKeys)
 	{
-		var style:CSSStyleDeclaration = document.createElement("div").style;
-		
-		for(var property of Object.keys(style))
+		for (var prefix of dom_prefixes)
 		{
-			for(var prefix of dom_prefixes) {
-				if(property.toLowerCase().startsWith(prefix)) {
-					vendorSpecificProperties.push(property.replace(prefix, ""))
-				}
-			}
+			if (property.toLowerCase().startsWith(prefix))
+				vendorSpecificProperties.push(property.replace(prefix, ""))
 		}
-		return vendorSpecificProperties;
 	}
+	return vendorSpecificProperties;
 }
 
-export default function prefixer(style:CSSProperties):CSSProperties {
-	
+export default function prefixer(style:CSSProperties):CSSProperties
+{
 	var newStyle:CSSProperties = {};
-	for(var property of Object.keys(style))
+	for (var property of Object.keys(style))
 	{
 		var prefixIndex = getVendorSpecificProperties().indexOf(property);
-		if(prefixIndex >= 0)
+		if (prefixIndex >= 0)
 		{
-			for(var prefix of react_prefixes)
-				newStyle[prefix+property] = style[property];
+			for (var prefix of react_prefixes)
+				newStyle[prefix + property] = style[property];
 		}
 		else
 		{
