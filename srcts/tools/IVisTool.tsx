96df05a7b2e3d14f45b0c8042e29920da89b8e9b
shwetapurushe <shwetapurushe@gmail.com>
2016-03-31 18:55:40 -0400

    new selectable attribute component design (according to new attr selection design)

--- srcts/tools/IVisTool.tsx
+++ srcts/tools/IVisTool.tsx
@@ -2,7 +2,7 @@ import ILinkableObject = weavejs.api.core.ILinkableObject;
 import WeavePath = weavejs.path.WeavePath;
 import * as React from "react";
 import SelectableAttributeComponent from "../ui/SelectableAttributeComponent";
-import SelectableAttributesList from "../ui/SelectableAttributesList";
+
 import ToolTip from "./ToolTip";
 import LinkableHashMap = weavejs.core.LinkableHashMap;
 import IColumnWrapper = weavejs.api.data.IColumnWrapper;
@@ -24,19 +24,5 @@ export interface IVisTool extends ILinkableObject
 
 export function renderSelectableAttributes(tool:IVisTool)
 {
-	var attrLabels = Array.from(tool.selectableAttributes.keys());
-
-	return attrLabels.map((label: string, index: number) => {
-		let attribute_lhm_or_icw = tool.selectableAttributes.get(label);
-		if (Weave.IS(attribute_lhm_or_icw, IColumnWrapper)) // IColumnWrapper
-		{
-			let attribute = attribute_lhm_or_icw as IColumnWrapper;
-			return <SelectableAttributeComponent key={index} attributeNames={attrLabels} label={label} attribute={attribute}/>;
-		}
-		else  // LinkableHashMap
-		{
-			let attribute = attribute_lhm_or_icw as LinkableHashMap;
-			return <SelectableAttributesList key={index} attributeNames={attrLabels} label={label} columns={attribute} showLabelAsButton={true}/>;
-		}
-	});
+	return(<SelectableAttributeComponent attributes={ tool.selectableAttributes }/>);
 }
--- srcts/ui/SelectableAttributeComponent.tsx
+++ srcts/ui/SelectableAttributeComponent.tsx
@@ -2,56 +2,114 @@ import * as React from "react";
 import {VBox, HBox} from '../react-ui/FlexBox';
 import AttributeSelector from "../ui/AttributeSelector";
 import classNames from "../modules/classnames";
+import PopupWindow from "../react-ui/PopupWindow";
+import SelectableAttributesList from "../ui/SelectableAttributesList";
+import StatefulComboBox from '../ui/StatefulComboBox';
 import ColumnUtils = weavejs.data.ColumnUtils;
 import IColumnWrapper = weavejs.api.data.IColumnWrapper;
-import PopupWindow from "../react-ui/PopupWindow";
+import LinkableHashMap = weavejs.core.LinkableHashMap;
+import IWeaveTreeNode = weavejs.api.data.IWeaveTreeNode;
+import ColumnMetadata = weavejs.api.data.ColumnMetadata;
+import IDataSource = weavejs.api.data.IDataSource;
+import HierarchyUtils = weavejs.data.hierarchy.HierarchyUtils;
+import ILinkableHashMap = weavejs.api.core.ILinkableHashMap;
 
-export interface ISelectableAttributeComponentProps
-{
-	attribute : IColumnWrapper;
-	label : string;
-	attributeNames?:string[];
+export interface ISelectableAttributeComponentProps{
+    attributes : Map<string, IColumnWrapper|LinkableHashMap>
 }
 
-export interface ISelectableAttributeComponentState
-{
+export interface ISelectableAttributeComponentState{
+
 }
+export default class SelectableAttributeComponent extends React.Component<ISelectableAttributeComponentProps, ISelectableAttributeComponentState>{
+    constructor (props:ISelectableAttributeComponentProps){
+        super(props);
+        let attrs = props.attributes.values();//using one selectable attribute to get the Weave root hashmap
 
-export default class SelectableAttributeComponent extends React.Component<ISelectableAttributeComponentProps, ISelectableAttributeComponentState>
-{
-	constructor(props:ISelectableAttributeComponentProps)
-	{
-		super(props);
-	}
+        //TODO find better way to get the root hashmap
+        props.attributes.forEach((value, key)=>{
+            this.weaveRoot = Weave.getRoot(value); return;
+        });
+        this.rootTreeNode = new weavejs.data.hierarchy.WeaveRootDataTreeNode(this.weaveRoot);
+    }
 
-	columnString: string;
+    private weaveRoot:ILinkableHashMap;
+    private rootTreeNode:IWeaveTreeNode;
+    columnString: string;
 
-	launchAttributeSelector=():PopupWindow=>{
-		return AttributeSelector.openInstance(this.props.label, this.props.attribute, this.props.attributeNames);
-	};
+  /*  launchAttributeSelector=():PopupWindow=>{
+        return AttributeSelector.openInstance(this.props.label, this.props.attribute, this.props.attributeNames);
+    };*/
 
-	componentDidMount()
-	{
-		Weave.getCallbacks(this.props.attribute).addGroupedCallback(this, this.forceUpdate)
-	}
-	//TODO figure out how to make an indent component
-	render():JSX.Element
-	{
-		var clearStyle = classNames({ 'fa fa-trash-o' : true, 'weave-icon' : true});
-		var labelStyle = {textAlign: 'center', flex: 0.35, fontSize: 'smaller'};
+     componentWillReceiveProps(props:ISelectableAttributeComponentProps){
 
-		this.columnString = ColumnUtils.getColumnListLabel(this.props.attribute);
+     }
 
-		//TODO return single instance
-		return (
-			<HBox className="weave-padded-hbox" style={{justifyContent: 'space-around', alignItems: 'center'}}>
+     siblings= (attribute:IColumnWrapper|LinkableHashMap): {label: string, value: IWeaveTreeNode}[]  =>{
+        var siblings:{label: string, value: IWeaveTreeNode}[];
 
-				<button style={ labelStyle } onClick={ this.launchAttributeSelector }>
-				{ Weave.lang(this.props.label) }</button>
+        var label = ColumnUtils.getColumnListLabel(attribute as IColumnWrapper);//TODO figure out how to use this
+        var dc = ColumnUtils.hack_findInternalDynamicColumn(attribute as IColumnWrapper);
 
-				<input style={{flex: 1}} type="text" value={ this.columnString } readOnly/>
-				<span className={clearStyle}/>
-			</HBox>
-		);
-	}
+        var meta = ColumnMetadata.getAllMetadata(dc);//gets metadata for a column
+
+        var dataSources = ColumnUtils.getDataSources(dc) as IDataSource[];
+        var dataSource = dataSources.length && dataSources[0];
+
+        //var columnNode = dataSource && dataSource.findHierarchyNode(meta);
+
+        if(dataSource){//if a column has not been set, datasource is not returned
+            var siblingNodes = HierarchyUtils.findParentNode(this.rootTreeNode, dataSource, meta).getChildren();
+
+            siblings = siblingNodes.map((value:IWeaveTreeNode, index:number)=>{
+                var label:string = value.getLabel();
+                return({label, value});
+            });
+        }
+
+        return siblings;
+     };
+
+
+
+    render():JSX.Element
+    {
+        //styles
+        var clearStyle = classNames({ 'fa fa-times-circle' : true, 'weave-icon' : true});
+        var labelStyle = {textAlign: 'center', flex: 0.35, fontSize: 'smaller'};
+        var btnStyle = {textAlign: 'center', flex: 0.05, fontSize: 'smaller'};
+
+        var selectableUI:JSX.Element[] = [];
+        let disabled:boolean = false;
+
+       //loop through selectable attributes
+       this.props.attributes.forEach((value, label)=>{
+           let attribute_lhm_or_icw = this.props.attributes.get(label);
+
+           if(Weave.IS(attribute_lhm_or_icw, IColumnWrapper)){
+               let attribute = attribute_lhm_or_icw as IColumnWrapper;
+
+               let siblings = this.siblings(attribute);
+               if(!siblings){//if no siblings are returned then disable combobox TODO Statefulcombobox should handle null option
+                   siblings = []; disabled = true;
+               }
+
+               let elem =      <HBox key={ label } className="weave-padded-hbox" style={{justifyContent: 'space-around', alignItems: 'center'}}>
+                                        <span style={ labelStyle }>{ Weave.lang(label) }</span>
+                                        <StatefulComboBox disabled={ disabled } style={{flex: 1}} options={ siblings }/>
+                                        <span className={clearStyle}/>
+                                        <button style={ btnStyle }>...</button>
+                               </HBox>;
+
+               selectableUI.push(elem);
+           }
+           else{//LinkableHashMap
+               let attribute = attribute_lhm_or_icw as LinkableHashMap;
+               let elem= <SelectableAttributesList key={ label } label={ label } columns={ attribute } showLabelAsButton={ true }/>;
+               selectableUI.push(elem);
+           }
+       });
+
+       return (<VBox>{selectableUI}</VBox>);
+    }
 }
