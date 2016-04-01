ba1d6b56207024e1d54e579efe21380f91a228bf
franckamayou <franckamayou@gmail.com>
2016-04-01 15:52:03 -0400

    created CenteredIcon component

---
+++ srcts/react-ui/CenteredIcon.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import * as _ from "lodash";
+
+export interface CenteredIconProps extends React.HTMLProps<HTMLSpanElement>
+{
+	iconProps?:React.HTMLProps<HTMLImageElement>
+}
+
+export interface CenteredIconState
+{
+	
+}
+
+export default class CenteredICon extends React.Component<CenteredIconProps, CenteredIconState>
+{
+	
+	constructor(props:CenteredIconProps)
+	{
+		super(props)
+	}
+
+
+	render() 
+	{
+		return (
+			<span className="weave-icon"  {...this.props} style={_.merge(this.props.style||{}, {alignSelf: "stretch", display: "flex", cursor: "pointer"})}>
+				{this.props.children || <i {...this.props.iconProps} style={_.merge(this.props.iconProps.style||{}, {alignSelf: "center"})}/>}
+			</span>
+		)
+	}
+}
