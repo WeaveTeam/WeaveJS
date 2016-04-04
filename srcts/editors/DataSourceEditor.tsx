5ef9adb3bbe103769ba20b87aa205ec973b8a63c
franckamayou <franckamayou@gmail.com>
2016-04-04 17:44:40 -0400

    added initial code for preview and metadata editor

--- srcts/editors/DataSourceEditor.tsx
+++ srcts/editors/DataSourceEditor.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as _ from "lodash";
 import {VBox, HBox} from "../react-ui/FlexBox";
 import ReactUtils from "../utils/ReactUtils";
 import StatefulTextField from "../ui/StatefulTextField";
@@ -16,30 +17,38 @@ import IAttributeColumn = weavejs.api.data.IAttributeColumn;
 import IQualifiedKey = weavejs.api.data.IQualifiedKey;
 import ColumnUtils = weavejs.data.ColumnUtils;
 
+export const PREVIEW = "preview";
+export const METADATA = "metadata";
+export const BROWSE = "browse";
+export type View = typeof PREVIEW | typeof METADATA | typeof BROWSE;
+
 export interface IDataSourceEditorProps {
 	dataSource: IDataSource;
 };
 
 export interface IDataSourceEditorState {
 	selectedNode?: IWeaveTreeNode;
+	view?: View
 };
 
 export default class DataSourceEditor extends React.Component<IDataSourceEditorProps, IDataSourceEditorState> 
 {
 	dataSourceWatcher:LinkableWatcher = Weave.disposableChild(this, new LinkableWatcher(IDataSource, null, this.forceUpdate.bind(this)));
 	columnWatcher:LinkableWatcher = Weave.disposableChild(this, new LinkableWatcher(IAttributeColumn, null, this.forceUpdate.bind(this)));
-
 	private tableContainer:VBox;
 	private tableContainerElement:HTMLElement;
 	protected tree:WeaveTree;
+
 	protected editorButtons:Map<React.ReactChild, Function>;
-	
+	private parentNode:IWeaveTreeNode;
+
 	constructor(props:IDataSourceEditorProps)
 	{
 		super(props);
 		this.componentWillReceiveProps(props);
 		this.state = {
-			selectedNode: props.dataSource.getHierarchyRoot()
+			selectedNode: props.dataSource.getHierarchyRoot(),
+			view: BROWSE
 		}
 	}
 	
@@ -77,21 +86,14 @@ export default class DataSourceEditor extends React.Component<IDataSourceEditorP
 			]
 		};
 
-		return (
-			<VBox>
-				<label> {Weave.lang("Edit {0}", this.props.dataSource.getHierarchyRoot().getLabel())} </label>
-				{
-					ReactUtils.generateTable(null, this.editorFields, tableStyles)
-				}
-			</VBox>
-		)
+		return ReactUtils.generateTable(null, this.editorFields, tableStyles);
 	}
 	
 	showColumns(selectedItems:IWeaveTreeNode[])
 	{
-		var node = selectedItems && selectedItems.length && selectedItems[0];
+		this.parentNode = selectedItems && selectedItems.length && selectedItems[0];
 		this.setState({
-			selectedNode: node
+			selectedNode: this.parentNode
 		});
 	}
 
@@ -125,12 +127,14 @@ export default class DataSourceEditor extends React.Component<IDataSourceEditorP
 			id: IQualifiedKey,
 			value: this.column
 		}, null, String);
+		
 		rows = rows.map((row) => {
 			return {
 				id: row.id.toString(),
 				value: row.value
 			}
 		});
+		
 		var keyType = this.column.getMetadata("keyType");
 		var dataType = this.column.getMetadata("dataType");
 		var columnIds = ["id", "value"];
@@ -155,35 +159,102 @@ export default class DataSourceEditor extends React.Component<IDataSourceEditorP
 	{
 		let root = this.props.dataSource.getHierarchyRoot();
 		return (
-			<HBox style={{flex: 1}}>
-				<VBox style={{flex: 1}}>
-					<WeaveTree initialSelectedItems={[this.state.selectedNode]} initialOpenItems={[root]} root={root} hideLeaves={true} onSelect={(selectedItems) => this.showColumns(selectedItems)}/>
-				</VBox>
-				<div style={{width: 10}}/>
-				{ 
-					this.state.selectedNode
-					?	
-					<VBox style={{flex: 1}}>
-					  <WeaveTree root={this.state.selectedNode} hideRoot={true} hideBranches={true} onSelect={(selectedItems) => this.updateColumnTarget(selectedItems)}/>
-	    			  {this.renderPreviewTable()}
-					</VBox>
-					: ""
+			<VBox style={{flex: 1}}>
+				{
+					this.renderFields()
 				}
-			</HBox>
+				<HBox style={{flex: 1}}>
+					<VBox style={{flex: 1}}>
+						<WeaveTree initialSelectedItems={[this.state.selectedNode]} initialOpenItems={[root]} root={root} hideLeaves={true} onSelect={(selectedItems) => this.showColumns(selectedItems)}/>
+					</VBox>
+					<div style={{width: 10}}/>
+					{ 
+						this.state.selectedNode
+						?	
+						<VBox style={{flex: 1}}>
+						  <WeaveTree root={this.state.selectedNode} hideRoot={true} hideBranches={true} onSelect={(selectedItems) => this.updateColumnTarget(selectedItems)}/>
+						  <HBox style={{justifyContent: "flex-end"}}>
+						  	<button onClick={() => this.setState({view: PREVIEW})}>{Weave.lang("Preview")}</button>
+							<div style={{width: 10}}/>
+							<button onClick={() => this.setState({view: METADATA})}>{Weave.lang("Edit Metadata")}</button>
+						  </HBox>
+		    			  {
+							  this.renderPreviewTable()
+						  }
+						</VBox>
+						: ""
+					}
+				</HBox>
+			</VBox>
+		);
+	}
+	
+	renderDataPreview():JSX.Element
+	{
+		var leaves = this.parentNode && this.parentNode.getChildren().filter((n) => !n.isBranch());
+		if(!leaves)
+			return;
+		var columns:IAttributeColumn[] = [];
+		for(var leaf of leaves)
+		{
+			var columnRef = Weave.AS(leaf, weavejs.api.data.IColumnReference);
+			if(columnRef)
+				columns.push(columnRef.getDataSource().getAttributeColumn(columnRef.getColumnMetadata()));
+		}
+		
+		var names:string[] = columns.map(column => column.getMetadata("title"));
+		var format:any = _.zipObject(names, columns);
+		var columnTitles = _.zipObject(names, names);
+
+		// // adding the qkey
+		// format[""] = IQualifiedKey;
+		// names.unshift("");
+		// (columnTitles as any)[""] = Weave.lang("Key");
+
+		//var rows = ColumnUtils.getRecords(columns, null, String);
+		return (
+			<VBox>
+				<FixedDataTable rows={[]} 
+								columnIds={names} 
+								idProperty="id"
+								showIdColumn={true}
+								columnTitles={columnTitles as any}/>
+				<button onClick={() => this.setState({view: BROWSE})}>{Weave.lang("Ok")}</button>
+			</VBox>
+		);
+	}
+	
+	renderMetadataView():JSX.Element
+	{
+		
+		return (
+			<VBox>
+				<button onClick={() => this.setState({view: BROWSE})}>Back</button>
+				Metadata view
+			</VBox>
 		);
 	}
 	
 	render():JSX.Element
 	{
+		var currentView:JSX.Element = null;
+		
+		if(this.state.view == BROWSE)
+			currentView = this.renderBrowseView()
+		else if(this.state.view == PREVIEW)
+			currentView = this.renderDataPreview()
+		else if (this.state.view == METADATA)
+			currentView = this.renderMetadataView()
+
 		return (
-			<VBox style={{flex:1}}>
+			<VBox style={{flex: 1}}>
+				<label> {Weave.lang("Edit {0}", this.props.dataSource.getHierarchyRoot().getLabel())} </label>
+				<VBox style={{flex:1}}>
 				{
-					this.renderFields()
+					currentView
 				}
-				{
-					this.renderBrowseView()
-				}
+				</VBox>
 			</VBox>
-		)
+		);
 	}
 };
