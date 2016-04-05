5f5b981e94f1c76ee25412bcc27c206dccce5273
shwetapurushe <shwetapurushe@gmail.com>
2016-04-04 21:37:37 -0400

    getting viz tools

--- srcts/tools/AttributeMenuTool/AttributeMenuTool.tsx
+++ srcts/tools/AttributeMenuTool/AttributeMenuTool.tsx
@@ -1,23 +1,26 @@
 import * as React from 'react';
 import {IVisTool,IVisToolState, IVisToolProps} from "../IVisTool";
+import {HBox, VBox} from "../../react-ui/FlexBox";
+import ReactUtils from "../../utils/ReactUtils";
+import StatefulComboBox from "../../ui/StatefulComboBox";
+
 import IColumnWrapper = weavejs.api.data.IColumnWrapper;
 import LinkableHashMap = weavejs.core.LinkableHashMap;
-import {HBox, VBox} from "../../react-ui/FlexBox";
 import LinkableVariable = weavejs.core.LinkableVariable;
 import ILinkableHashMap = weavejs.api.core.ILinkableHashMap;
 import WeaveAPI = weavejs.WeaveAPI;
+
 export interface IAttributeMenuToolState extends IVisToolState{
 
 }
 
 export default class AttributeMenuTool extends React.Component<IVisToolProps, IAttributeMenuToolState> implements IVisTool
 {
-    targetToolPath = Weave.linkableChild(this, new LinkableVariable(Array));
+    private openTools:any [];
     private weaveRoot :ILinkableHashMap;
     constructor (props:IVisToolProps){
         super(props);
-        console.log(props);
-
+        this.state = {};
         WeaveAPI.Scheduler.callLater(this, this.getOpenVizTools);
     }
 
@@ -26,25 +29,48 @@ export default class AttributeMenuTool extends React.Component<IVisToolProps, IA
     }
 
     getOpenVizTools=():void =>{
-        var tools:any[];
         this.weaveRoot = Weave.getRoot(this);
+        this.openTools = [];
 
-        tools = this.weaveRoot.getObjects().filter((tool:any)=>{return tool.selectableAttributes;});//returns IVisTools
+        this.weaveRoot.getObjects().forEach((tool:any):void=>{ if(tool.selectableAttributes)
+            this.openTools.push(this.weaveRoot.getName(tool));
+        });
+
     };
+
 
     get selectableAttributes()
     {
         return new Map<string, IColumnWrapper | LinkableHashMap>();
     }
 
-    renderEditor():JSX.Element{
-        return(<VBox>
+    get toolConfigs():[string, JSX.Element][]{
+        return[
+            [
+                Weave.lang("Visualization Tool"),
+                <StatefulComboBox options={ this.openTools }/>
+            ]
+        ];
+    }
 
+    renderEditor():JSX.Element{
+        var tableStyles = {
+            table: { width: "100%", fontSize: "inherit" },
+            td: [
+                { paddingBottom: 10, textAlign: "right", whiteSpace: "nowrap", paddingRight: 5 },
+                { paddingBottom: 10, textAlign: "right", width: "100%" }
+            ]
+        };
+
+        return(<VBox>
+            { this.openTools ? ReactUtils.generateTable(null, this.toolConfigs, tableStyles): null}
         </VBox>);
     }
 
     render():JSX.Element{
-        return(<VBox></VBox>);
+
+        return(<VBox>
+        </VBox>);
     }
 }
 Weave.registerClass('weavejs.tool.AttributeMenu', AttributeMenuTool, [weavejs.api.ui.IVisTool],'Attribute Menu Tool' );
