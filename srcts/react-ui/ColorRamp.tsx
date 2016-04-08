d3cfc68f7b690fb6ab5bed0172f683525e17c028
Zachary Maybury <maybury@iweave.com>
2016-04-08 14:40:30 -0400

    Generic color ramp for array of hex strings #WEAVE-281 Fixed Spent time 1h

---
+++ srcts/react-ui/ColorRamp.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import * as _ from "lodash";
+
+export interface ColorRampProps extends React.HTMLProps<HTMLDivElement>
+{
+	ramp:string[];
+	direction?:string;
+}
+
+export interface ColorRampState
+{
+}
+
+export default class ColorRamp extends React.Component<ColorRampProps, ColorRampState>
+{
+
+	constructor(props:ColorRampProps) {
+		super(props);
+	}
+
+	render():JSX.Element {
+		var direction:string = this.props.direction ? this.props.direction:"to right";
+		var rampStyle:React.CSSProperties = {
+			background: "linear-gradient("+  direction + "," + this.props.ramp.join(", ") + ")",
+			border: '1px'
+		};
+
+		return (<div {...this.props} style={_.merge(this.props.style,rampStyle)}/>);
+	}
+}
