8679774364c4344f05450c1021c18bf8a7344374
Philip Kovac <philip.kovac@iweave.com>
2016-04-13 16:45:19 -0400

    Simplified SelectableAttributeComponent; fixed NPE in AttributeSelector; added equalityfunc and onclick props to Dropdown

--- srcts/ui/SelectableAttributeComponent.tsx
+++ srcts/ui/SelectableAttributeComponent.tsx
@@ -143,13 +143,6 @@ interface IAttributeDropdownProps extends React.HTMLProps<AttributeDropdown> {
 interface IAttributeDropdownState {
 }
 
-type LocalColumnMetadata = {[attr:string]: string};
-
-type JSONColumnReference = {
-    dataSource: string;
-    metadata: LocalColumnMetadata;
-}
-
 class AttributeDropdown extends React.Component<IAttributeDropdownProps, IAttributeDropdownState>{
     constructor(props:IAttributeDropdownProps){
         super(props);
@@ -169,82 +162,56 @@ class AttributeDropdown extends React.Component<IAttributeDropdownProps, IAttrib
         this.props.attribute.removeCallback(this, this.forceUpdate);//forceUpdate keeps being called when component is unmounted
     }
 
-    state: IAttributeDropdownState = {selectedOption: null};
+    state: IAttributeDropdownState = {};
     private click:(event:React.MouseEvent)=>PopupWindow = null;
 
-    siblings=(attribute:IColumnWrapper): {label: string, id:string}[] =>
+    onChange = (columnReference: IColumnReference & IWeaveTreeNode) => 
     {
-        var siblings:{label: string, id:string}[];
-        var colRef = ColumnUtils.hack_findHierarchyNode(attribute);
-        if (!colRef)
-            return [];
-        var siblingNodes = HierarchyUtils.findSiblingNodes(colRef.getDataSource(), colRef.getColumnMetadata());
-        return siblingNodes.filter((node:IWeaveTreeNode) => Weave.IS(node, IColumnReference)).map((node:IWeaveTreeNode) => {
-            return {label: node.getLabel(), id: this.getColumnReferenceString(node)};
-        });
-    };
+        if (columnReference)
+        {
+            let internalReferencedColumn = this.props.attribute.requestLocalObject(ReferencedColumn) as ReferencedColumn;
+            internalReferencedColumn.setColumnReference(columnReference.getDataSource(), columnReference.getColumnMetadata());
+        }
+        else
+        {
+            this.props.attribute.removeObject();
+        }
+    }
 
-    getColumnReferenceString=(node:IWeaveTreeNode|IColumnWrapper|IColumnReference):string=>
+    private static nodeEqualityFunc(a:IColumnReference&IWeaveTreeNode, b:IColumnReference&IWeaveTreeNode):boolean
     {
-        var metadata:{[attr:string]:string};
-        // get IWeaveTreeNode from IColumnWrapper, which will also be an IColumnReference
-        if (Weave.IS(node, IColumnWrapper))
-            node = ColumnUtils.hack_findHierarchyNode(node as IColumnWrapper);
-
-        if (!node)
-            return null;
-        let colRef = Weave.AS(node, IColumnReference);
-        if (!colRef)
-            return null;
-        let dataSource = colRef.getDataSource();
-        let dataSourceName = (Weave.getOwner(dataSource) as ILinkableHashMap).getName(dataSource);
-
-        return Weave.stringify({dataSource: dataSourceName, metadata: colRef.getColumnMetadata()});
-    };
-
-    getColumnReference = (columnReferenceString: string):IColumnReference=>
-    {
-        let jsonColumnReference = JSON.parse(columnReferenceString) as JSONColumnReference;
-        let dataSource = Weave.getRoot(this.props.attribute).getObject(jsonColumnReference.dataSource) as IDataSource;
-        return Weave.AS(dataSource.findHierarchyNode(jsonColumnReference.metadata), IColumnReference);
-    };
-
-    onChange=(event:React.FormEvent)=>
-    {
-        let columnString = (event.target as HTMLSelectElement).value;
-        let columnReference = this.getColumnReference(columnString);
-        this.props.attribute
-            .requestLocalObject(ReferencedColumn)
-            .setColumnReference(columnReference.getDataSource(), columnReference.getColumnMetadata());
-    };
+        if (a && b)
+            return a.equals(b)
+        else
+            return (a === b);
+    }
 
     render():JSX.Element{
-        var options:JSX.Element[];
-        var disabled:boolean = false;
 
-        let currentColumnEntryId = this.getColumnReferenceString(this.props.attribute);
-        let columnEntry = {label: this.props.attribute.getMetadata(ColumnMetadata.TITLE), id: currentColumnEntryId};
-
-        let siblings = this.siblings(this.props.attribute);
-        if(siblings)
-            options = siblings.map((option:{label:string, id:string}, index:number)=>{
-                return(<option value={ option.id } key={ option.id }>{ option.label }</option>);
-            });
-
-        //register the click handler only when options are absent
-        if(!options || options.length ==0 ){
-            var defaultEntry = <option key={ 'def' }>Click here to select</option>;
-            options = [defaultEntry];
-            //disabled = true;
-            this.click = this.props.clickHandler;
-        }
-        else{//once siblings retrieved , unregister it
-            this.click = null;
+        let currentColumnNode = ColumnUtils.hack_findHierarchyNode(this.props.attribute);
+        let options:{value: IColumnReference&IWeaveTreeNode, label: string}[] = [];
+        let clickHandler = this.props.clickHandler;
+        if (currentColumnNode)
+        {
+            clickHandler = null;
+            options = 
+                HierarchyUtils.findSiblingNodes(currentColumnNode.getDataSource(), currentColumnNode.getColumnMetadata())
+                .filter((node:IWeaveTreeNode)=> Weave.IS(node, IColumnReference))
+                .map((node) =>
+                {
+                    let colRef = Weave.AS(node, IColumnReference) as IColumnReference&IWeaveTreeNode;
+                    let metadata = colRef.getColumnMetadata() as any;
+                    let title:string = metadata[ColumnMetadata.TITLE];
+                    return {value: colRef, label: title};
+                });
         }
 
-        return(<VBox style={ this.props.style }>
-            <select disabled={ disabled } value={ columnEntry.id } onClick={ this.click } onChange={ this.onChange } >
-                { options }</select>
-        </VBox>);
+        options.push({ value: null, label: Weave.lang("(None)") });
+
+        return <Dropdown valueEqualityFunc={AttributeDropdown.nodeEqualityFunc}
+            value={currentColumnNode}
+            onClick={clickHandler}
+            options={options}
+            onChange={this.onChange}/>;
     }
 }
