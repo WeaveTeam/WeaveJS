0b5e4fed38195cc56d7a1126ec90c3d76f2d885e
Franck Kamayou <franckamayou@gmail.com>
2016-04-13 08:26:47 -0400

    fixed issue with FileSelector component

--- srcts/ui/FileSelector.tsx
+++ srcts/ui/FileSelector.tsx
@@ -6,6 +6,8 @@ import ReactUtils from "../utils/ReactUtils";
 import WeaveTree from "../ui/WeaveTree";
 import {HBox, VBox} from "../react-ui/FlexBox";
 import Input from "../semantic-ui/Input";
+import Button from "../semantic-ui/Button";
+
 import LinkableFile = weavejs.core.LinkableFile;
 import LinkableString = weavejs.core.LinkableString;
 var URLRequestUtils = weavejs.WeaveAPI.URLRequestUtils;
@@ -28,10 +30,6 @@ export default class LinkableFileSelector extends React.Component<IFileSelectorP
 	{
 		super(props);
 	}
-	textField:StatefulTextField;
-	componentDidMount()
-	{
-	}
 	
 	handleFileChange=(event:React.FormEvent)=>
 	{
@@ -44,7 +42,6 @@ export default class LinkableFileSelector extends React.Component<IFileSelectorP
 			let buffer = reader.result as ArrayBuffer;
 			let fileName = URLRequestUtils.saveLocalFile(Weave.getRoot(this.props.targetUrl), file.name, new Uint8Array(buffer));
 			this.props.targetUrl.value = fileName;
-			// this.textField.input.inputElement.blur(); // clear the blur
 		}
 
 		reader.readAsArrayBuffer(file);
@@ -52,19 +49,22 @@ export default class LinkableFileSelector extends React.Component<IFileSelectorP
 
 	render():JSX.Element
 	{
+		var hBoxFlex:React.CSSProperties = {};
+		if(this.props.style && this.props.style.flex)
+			hBoxFlex.flex = this.props.style.flex;
+		
+		// find a way to not include the border color in this file.
 		return (
+			<HBox style={hBoxFlex}>
 				<StatefulTextField {...this.props}
-								   className={"right labeled " + (this.props.className || "")}
-								   placeholder={this.props.placeholder}
-							   	   ref={linkReactStateRef(this, {value: this.props.targetUrl}, 500)}>
-				   {
-					   <div className="ui label">
-					   		<FileInput onChange={this.handleFileChange} accept={this.props.accept}>
-						   		{Weave.lang("Add file")}
-					   		</FileInput>
-						</div>
-				   }
-				</StatefulTextField>
+									className={"right labeled " + (this.props.className || "")}
+									ref={linkReactStateRef(this, {value: this.props.targetUrl}, 500)}/>
+				<FileInput onChange={this.handleFileChange} accept={this.props.accept}>
+					<Button style={{borderTopLeftRadius: 0, borderBottomLeftRadius: 0, margin: 0, whiteSpace: "nowrap", border: "1px solid #E0E1E2"}}>
+						{Weave.lang("Add file")}
+					</Button>
+				</FileInput>
+			</HBox>
 		)
 	}
 }
