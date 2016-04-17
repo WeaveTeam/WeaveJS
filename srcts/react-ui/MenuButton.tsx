419e56186ccdbc314f7b96f19035a225f4019ca9
Andy Dufilie <andrew.dufilie@iweave.com>
2016-04-17 02:10:50 -0400

    fixed MenuButton logic bug and allow styling

--- srcts/react-ui/MenuButton.tsx
+++ srcts/react-ui/MenuButton.tsx
@@ -1,14 +1,15 @@
 import * as React from "react";
 import * as ReactDOM from 'react-dom';
+import * as _ from 'lodash';
 import Menu from "./Menu";
 import {MenuItemProps} from "./Menu";
 import ReactUtils from "../utils/ReactUtils";
 import Button from "../semantic-ui/Button";
 
-export interface MenuButtonProps extends React.Props<MenuButton>
+export interface MenuButtonProps extends React.HTMLProps<MenuButton>
 {
 	menu:MenuItemProps[];
-	showHamburgerIcon?:boolean;
+	showIcon?:boolean;
 }
 
 export interface MenuButtonState
@@ -25,15 +26,19 @@ export default class MenuButton extends React.Component<MenuButtonProps, MenuBut
 		super(props)
 	}
 
-	static defaultProps:MenuButtonProps = { showHamburgerIcon : true, menu:[] };
+	static defaultProps:MenuButtonProps = {
+		showIcon: true,
+		menu: []
+	};
 
-	componentDidMount (){
+	componentDidMount()
+	{
 		this.element = ReactDOM.findDOMNode(this) as HTMLElement;
 	}
 
 	closeMenu=()=>
 	{
-		if(this.menu)
+		if (this.menu)
 		{
 			ReactUtils.closePopup(this.menu);
 			this.menu = null;
@@ -43,17 +48,17 @@ export default class MenuButton extends React.Component<MenuButtonProps, MenuBut
 	openMenu=()=>
 	{
 		var clientRect = this.element.getBoundingClientRect();
-		// close the popup if it was already open
-		if(this.menu)
+		// close the popup if it is already open
+		if (this.menu && this.menu.element && this.menu.element.parentNode)
 		{
 			this.closeMenu();
 		}
 		else
 		{
 			this.menu = ReactUtils.openPopup(
-				<Menu menu={this.props.menu} 
+				<Menu menu={this.props.menu}
 					xPos={clientRect.left} 
-					yPos={clientRect.top+this.element.clientHeight}
+					yPos={clientRect.top + this.element.clientHeight}
 					onClick={this.closeMenu}
 				/>,
 				true
@@ -63,9 +68,13 @@ export default class MenuButton extends React.Component<MenuButtonProps, MenuBut
 	
 	render():JSX.Element
 	{
+		var props = _.clone(this.props);
+		delete props.menu;
+		delete props.showIcon;
 		return (
-			<Button onMouseDown={this.openMenu}>
-				{ this.props.showHamburgerIcon ? <i className="fa fa-bars"/> : '' }
+			<Button {...props as any} onMouseDown={this.openMenu}>
+				{ this.props.showIcon ? <i className="fa fa-bars"/> : '' }
+				{ ' ' }
 				{ this.props.children }
 			</Button>
 		);
