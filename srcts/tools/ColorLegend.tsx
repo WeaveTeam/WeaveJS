7cd0bf50cf35a093b37d310b92ce890c1a0b0029
Andy Dufilie <andrew.dufilie@iweave.com>
2016-04-21 17:50:39 -0400

    color legend margin bottom when no binning

--- srcts/tools/ColorLegend.tsx
+++ srcts/tools/ColorLegend.tsx
@@ -384,12 +384,12 @@ export default class ColorLegend extends React.Component<IVisToolProps, IVisTool
 		else
 		{
 			//Continuous plot case
-			if(this.colorColumn)
+			if (this.colorColumn)
 			{
 				var dataColumn = this.colorColumn.internalDynamicColumn;
 				
 				return (
-					<VBox style={{flex: 1, marginLeft: 20}} className="weave-padded-vbox">
+					<VBox style={{flex: 1, marginLeft: 20, marginBottom: 8}} className="weave-padded-vbox">
 						<label style={{marginTop: 5, fontWeight: "bold"}}>{Weave.lang(this.dynamicColorColumn.getMetadata('title'))}</label>
 						<HBox style={{flex: 1, overflow: "auto"}} className="weave-padded-hbox">
 							<ColorRampComponent style={{width: 30}} direction="bottom" ramp={this.colorColumn ? this.colorColumn.ramp.getHexColors():[]}/>
