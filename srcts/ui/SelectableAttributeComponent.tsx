b0fc9c76dff140c9b881988125e0fa2551da435f
Andy Dufilie <andrew.dufilie@iweave.com>
2016-04-21 22:41:01 -0400

    spaces to tabs

--- srcts/ui/SelectableAttributeComponent.tsx
+++ srcts/ui/SelectableAttributeComponent.tsx
@@ -29,212 +29,239 @@ import DynamicColumn = weavejs.data.column.DynamicColumn;
 export interface ISelectableAttributeComponentProps
 {
 	attributeName: string;
-    attributes: Map<string, IColumnWrapper|ILinkableHashMap>
-    linkToToolEditorCrumb?:Function
+	attributes: Map<string, IColumnWrapper|ILinkableHashMap>
+	linkToToolEditorCrumb?:Function
 }
 
-export interface ISelectableAttributeComponentState{
-
+export interface ISelectableAttributeComponentState
+{
 }
-export default class SelectableAttributeComponent extends React.Component<ISelectableAttributeComponentProps, ISelectableAttributeComponentState>{
-    constructor (props:ISelectableAttributeComponentProps){
-        super(props);
-    }
 
-    static defaultProps = { showLabel: true };
+export default class SelectableAttributeComponent extends React.Component<ISelectableAttributeComponentProps, ISelectableAttributeComponentState>
+{
+	constructor (props:ISelectableAttributeComponentProps)
+	{
+		super(props);
+	}
 
-    componentWillReceiveProps(props:ISelectableAttributeComponentProps){
+	static defaultProps = { showLabel: true };
 
-    }
+	componentWillReceiveProps(props:ISelectableAttributeComponentProps)
+	{
+	}
 
-    componentDidMount(){
-        this.props.attributes.forEach((value, label)=>{
-            //TODO Do we add a check for value being an ILinkableObject?
-            Weave.getCallbacks(value).addGroupedCallback(this, this.forceUpdate);
-        });
-    }
-    
-	launchAttributeSelector=(label:string, attribute:IColumnWrapper|ILinkableHashMap):ControlPanel=>{
-        if(this.props.linkToToolEditorCrumb)
-        {
-            this.props.linkToToolEditorCrumb("Attributes", <AttributeSelector label={ label }
-                                                                             selectedAttribute={ attribute }
-                                                                             selectableAttributes = { this.props.attributes }/> )
-            return null;
-        }
-        else
-        {
-            return AttributeSelector.openInstance(label, attribute, this.props.attributes);
-        }
+	componentDidMount()
+	{
+		this.props.attributes.forEach((value, label)=>{
+			//TODO Do we add a check for value being an ILinkableObject?
+			Weave.getCallbacks(value).addGroupedCallback(this, this.forceUpdate);
+		});
+	}
+	
+	launchAttributeSelector=(label:string, attribute:IColumnWrapper|ILinkableHashMap):ControlPanel=>
+	{
+		if (this.props.linkToToolEditorCrumb)
+		{
+			this.props.linkToToolEditorCrumb(
+				"Attributes",
+				<AttributeSelector
+					label={ label }
+					selectedAttribute={ attribute }
+					selectableAttributes={ this.props.attributes }
+				/>
+			);
+			return null;
+		}
+		else
+		{
+			return AttributeSelector.openInstance(label, attribute, this.props.attributes);
+		}
 
-    };
+	};
 
-    private clearColumn =( attr:IColumnWrapper,event:MouseEvent):void =>{
-        var dc:any;//TODO assign right type
-        if(Weave.IS(attr, AlwaysDefinedColumn))
-            dc = ColumnUtils.hack_findInternalDynamicColumn(attr);
-        else
-            dc = attr;
-        dc.target = null;
+	private clearColumn =( attr:IColumnWrapper,event:MouseEvent):void =>
+	{
+		var dc:any;//TODO assign right type
+		if (Weave.IS(attr, AlwaysDefinedColumn))
+			dc = ColumnUtils.hack_findInternalDynamicColumn(attr);
+		else
+			dc = attr;
+		dc.target = null;
 
-    };
-    
+	};
+	
 
-    render():JSX.Element
-    {
-        
-        var selectableUI:JSX.Element[][] = [];
-        var listUI:JSX.Element[] = [];
-        let alwaysDefinedCol:boolean;
-        let defaultValue:any;
+	render():JSX.Element
+	{
+		var selectableUI:JSX.Element[][] = [];
+		var listUI:JSX.Element[] = [];
+		let alwaysDefinedCol:boolean;
+		let defaultValue:any;
 
 		let attribute_ilhm_or_icw = this.props.attributes.get(this.props.attributeName);
 
-        if(Weave.IS(attribute_ilhm_or_icw, IColumnWrapper)){
-            let attribute = attribute_ilhm_or_icw as IColumnWrapper;
+		if (Weave.IS(attribute_ilhm_or_icw, IColumnWrapper))
+		{
+			let attribute = attribute_ilhm_or_icw as IColumnWrapper;
 
-            let dropDownStyle:React.CSSProperties = {
-                borderBottomRightRadius:0,
-                borderTopRightRadius:0
+			let dropDownStyle:React.CSSProperties = {
+				borderBottomRightRadius:0,
+				borderTopRightRadius:0
 
-            };
+			};
 
-            let buttonStyle:React.CSSProperties = {
-                borderBottomLeftRadius:0,
-                borderTopLeftRadius:0,
-                borderLeft:"none"
-            };
+			let buttonStyle:React.CSSProperties = {
+				borderBottomLeftRadius:0,
+				borderTopLeftRadius:0,
+				borderLeft:"none"
+			};
 
-            return (
+			return (
 				<HBox style={ {flex: 1} }>
-					<AttributeDropdown title="Change column" style={ dropDownStyle }
-									   attribute={ ColumnUtils.hack_findInternalDynamicColumn(attribute) }
-									   clickHandler={ this.launchAttributeSelector.bind(this, this.props.attributeName, attribute) }/>
-					<Button onClick={ this.launchAttributeSelector.bind(this, this.props.attributeName, attribute) }
-                            style={buttonStyle}
-                            title={"Click to explore other DataSources for " + this.props.attributeName}>
-                        <i className="fa fa-angle-right" aria-hidden="true" style={ {fontWeight:"bold"} }/>
+					<AttributeDropdown
+						title="Change column" style={ dropDownStyle }
+						attribute={ ColumnUtils.hack_findInternalDynamicColumn(attribute) }
+						clickHandler={ this.launchAttributeSelector.bind(this, this.props.attributeName, attribute) }
+					/>
+					
+					<Button
+						onClick={ this.launchAttributeSelector.bind(this, this.props.attributeName, attribute) }
+						style={buttonStyle}
+						title={"Click to explore other DataSources for " + this.props.attributeName}
+					>
+						<i className="fa fa-angle-right" aria-hidden="true" style={ {fontWeight:"bold"} }/>
 					</Button>
 				</HBox>
-			)
-        }
-
-        else if(Weave.IS(attribute_ilhm_or_icw, ILinkableHashMap)){//LinkableHashMap
-            let attribute = attribute_ilhm_or_icw as ILinkableHashMap;
-
-            return (
-				<SelectableAttributesList key={this.props.attributeName} 
-													showAsList={false}
-													label={this.props.attributeName} 
-	                                                columns={attribute} 
-	                                                linkToToolEditorCrumb={this.props.linkToToolEditorCrumb}
-	                                                selectableAttributes={ this.props.attributes }/>
 			);
-        }
-    }
+		}
+		else if (Weave.IS(attribute_ilhm_or_icw, ILinkableHashMap))
+		{
+			let attribute = attribute_ilhm_or_icw as ILinkableHashMap;
+			return (
+				<SelectableAttributesList
+					key={this.props.attributeName} 
+					showAsList={false}
+					label={this.props.attributeName} 
+					columns={attribute} 
+					linkToToolEditorCrumb={this.props.linkToToolEditorCrumb}
+					selectableAttributes={this.props.attributes}
+				/>
+			);
+		}
+	}
 }
-
 
 
 //Custom component to handle drop down selection of columns as well as when selection from attribute selector
 //ComboBox allows selection only from drop down, Does not handle case of attribute selector
-interface IAttributeDropdownProps extends React.HTMLProps<AttributeDropdown> {
-    attribute:DynamicColumn;
-    clickHandler:(event:React.MouseEvent)=>PopupWindow;
+interface IAttributeDropdownProps extends React.HTMLProps<AttributeDropdown>
+{
+	attribute:DynamicColumn;
+	clickHandler:(event:React.MouseEvent)=>PopupWindow;
 }
 
-interface IAttributeDropdownState {
+interface IAttributeDropdownState
+{
 }
 
-class AttributeDropdown extends React.Component<IAttributeDropdownProps, IAttributeDropdownState>{
-    constructor(props:IAttributeDropdownProps){
-        super(props);
-    }
+class AttributeDropdown extends React.Component<IAttributeDropdownProps, IAttributeDropdownState>
+{
+	constructor(props:IAttributeDropdownProps)
+	{
+		super(props);
+	}
 
-    componentWillReceiveProps(nextProps:IAttributeDropdownProps)
-    {
-        if (!this.props || (this.props.attribute != nextProps.attribute))
-        {
-            //this.props.attribute.removeCallback(this, this.forceUpdate);//when props are replaced, old callbacks removed
-            //nextProps.attribute.addGroupedCallback(this, this.forceUpdate);
-        }
-    }
+	componentWillReceiveProps(nextProps:IAttributeDropdownProps)
+	{
+		if (!this.props || (this.props.attribute != nextProps.attribute))
+		{
+			//this.props.attribute.removeCallback(this, this.forceUpdate);//when props are replaced, old callbacks removed
+			//nextProps.attribute.addGroupedCallback(this, this.forceUpdate);
+		}
+	}
 
-    componentWillUnmount(){
-        this.props.attribute.removeCallback(this, this.forceUpdate);//forceUpdate keeps being called when component is unmounted
-    }
+	componentWillUnmount()
+	{
+		this.props.attribute.removeCallback(this, this.forceUpdate);//forceUpdate keeps being called when component is unmounted
+	}
 
-    state: IAttributeDropdownState = {};
-    private click:(event:React.MouseEvent)=>PopupWindow = null;
+	state: IAttributeDropdownState = {};
+	private click:(event:React.MouseEvent)=>PopupWindow = null;
 
-    onChange = (columnReference: IColumnReference & IWeaveTreeNode) => 
-    {
-        if (this.comboBox && ReactUtils.hasFocus(this.comboBox)) {
-            if (columnReference) {
-                let internalReferencedColumn = this.props.attribute.requestLocalObject(ReferencedColumn) as ReferencedColumn;
-                internalReferencedColumn.setColumnReference(columnReference.getDataSource(), columnReference.getColumnMetadata());
-            }
-            else {
-                this.props.attribute.removeObject();
-            }
-        }
-    }
+	onChange = (columnReference: IColumnReference & IWeaveTreeNode) => 
+	{
+		if (this.comboBox && ReactUtils.hasFocus(this.comboBox))
+		{
+			if (columnReference)
+			{
+				let internalReferencedColumn = this.props.attribute.requestLocalObject(ReferencedColumn) as ReferencedColumn;
+				internalReferencedColumn.setColumnReference(columnReference.getDataSource(), columnReference.getColumnMetadata());
+			}
+			else
+			{
+				this.props.attribute.removeObject();
+			}
+		}
+	}
 
-    private static nodeEqualityFunc(a:IColumnReference&IWeaveTreeNode, b:IColumnReference&IWeaveTreeNode):boolean
-    {
-        if (a && b)
-            return a.equals(b)
-        else
-            return (a === b);
-    }
+	private static nodeEqualityFunc(a:IColumnReference&IWeaveTreeNode, b:IColumnReference&IWeaveTreeNode):boolean
+	{
+		if (a && b)
+			return a.equals(b)
+		else
+			return (a === b);
+	}
 
-    private comboBox: ComboBox;
+	private comboBox: ComboBox;
 
-    render():JSX.Element{
+	render():JSX.Element
+	{
+		let node = ColumnUtils.hack_findHierarchyNode(this.props.attribute);
+		let options:{value: IColumnReference, label: string}[] = [];
+		let clickHandler = this.props.clickHandler;
+		let header:JSX.Element;
+		if (node)
+		{
+			let rootNode = node.getDataSource().getHierarchyRoot();
+			let parentNode = HierarchyUtils.findParentNode(rootNode, node.getDataSource(), node.getColumnMetadata());
+			let siblingNodes = parentNode && parentNode.getChildren();	
+			if (siblingNodes)
+			{
+				header = <span style={{ fontWeight: "bold", fontSize: "small" }}>{ parentNode.getLabel() }</span>;
+				clickHandler = null;
+				options = siblingNodes.map(
+					(node) => {
+						let colRef = Weave.AS(node, IColumnReference);
+						if (!colRef)
+							return null;
+						let metadata = colRef.getColumnMetadata() as any;
+						if (!metadata)
+							return null;
+						let title:string = metadata[ColumnMetadata.TITLE];
+						return {value: colRef, label: title};
+					}
+				).filter(_.identity);
+			}
+			else
+			{
+				[{ value: node, label: "Currently selected" }];
+			}
+		}
 
-        let currentColumnNode = ColumnUtils.hack_findHierarchyNode(this.props.attribute);
-        let options:{value: IColumnReference, label: string}[] = [];
-        let clickHandler = this.props.clickHandler;
-        let header:JSX.Element;
-        if (currentColumnNode)
-        {
-            let parentNode = HierarchyUtils.findParentNode(currentColumnNode.getDataSource().getHierarchyRoot(), currentColumnNode.getDataSource(), currentColumnNode.getColumnMetadata());
-            if (parentNode && parentNode.getChildren())
-            {
-                let siblings = parentNode.getChildren();    
-                header = (
-                    <span style={{ fontWeight: "bold", fontSize: "small" }}>
-                        {parentNode.getLabel() }</span>)
+		options.push({ value: null, label: Weave.lang("(None)") });
 
-                clickHandler = null;
-                options = 
-                    siblings
-                    .map((node) =>
-                    {
-                        let colRef = Weave.AS(node, IColumnReference);
-                        if (!colRef) return null;
-                        let metadata = colRef.getColumnMetadata() as any;
-                        if (!metadata) return null;
-                        let title:string = metadata[ColumnMetadata.TITLE];
-                        return {value: colRef, label: title};
-                    }).filter(_.identity);
-            }
-            else
-            {
-                [{ value: currentColumnNode, label: "Currently selected" }];
-            }
-        }
-
-        options.push({ value: null, label: Weave.lang("(None)") });
-
-        return <ComboBox ref={(c:ComboBox) => this.comboBox = c} valueEqualityFunc={AttributeDropdown.nodeEqualityFunc}
-                         style={ this.props.style }
-                         value={currentColumnNode}
-                         onClick={clickHandler}
-                         options={options}
-                         onChange={this.onChange}
-                         header={header}
-                         optionStyle={{marginLeft:10}}
-        />;
-    }
+		return (
+			<ComboBox
+				ref={(c:ComboBox) => this.comboBox = c}
+				valueEqualityFunc={AttributeDropdown.nodeEqualityFunc}
+				style={this.props.style}
+				value={node}
+				onClick={clickHandler}
+				options={options}
+				onChange={this.onChange}
+				header={header}
+				optionStyle={{marginLeft:10}}
+			/>
+		);
+	}
 }
