22d886c1453b4a3bb59620bd9d5d8b0597f7861c
Philip Kovac <philip.kovac@iweave.com>
2016-04-27 17:35:14 -0400

    Remove swfobject-amd-js from build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 /*global module */
 
-var libraries = ['react', 'react-dom', 'jquery', 'lodash', 'd3', 'c3', 'openlayers', 'jszip', 'swfobject-amd'];
+var libraries = ['react', 'react-dom', 'jquery', 'lodash', 'd3', 'c3', 'openlayers', 'jszip'];
 var libraries_colon = libraries.map(function (d) { return d + ":"});
 
 module.exports = function (grunt) {
