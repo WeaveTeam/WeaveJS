d218479f9274358fe9c4b651a9261dee21ee68b2
Zachary Maybury <maybury@iweave.com>
2016-05-17 16:18:22 -0400

    Initial weave server file list, opening server sessions, and drop zone for local files.
    Todo: Add browse button to add local file
    Todo: Better layout/style and authentication popup
    #Weave-498 Spent time 1d4h

---
+++ srcts/ui/FileInfoView.tsx
@@ -0,0 +1,61 @@
+import * as React from "react";
+import {HBox, VBox} from "../react-ui/FlexBox";
+
+import WeaveFileInfo = weavejs.net.beans.WeaveFileInfo;
+import ReactUtils from "../utils/ReactUtils";
+
+export interface IFileInfoViewProps extends React.Props<FileInfoView>
+{
+	fileInfo:WeaveFileInfo;
+}
+
+export interface IFileInfoViewState
+{
+}
+
+export default class FileInfoView extends React.Component<IFileInfoViewProps, IFileInfoViewState> {
+
+	constructor(props:IFileInfoViewProps)
+	{
+		super(props);
+	}
+
+	render():JSX.Element
+	{
+		let thumbnail:Blob = this.props.fileInfo && this.props.fileInfo.thumb && new Blob([this.props.fileInfo.thumb.data], { type: "image/jpeg" });
+		var tableStyles = {
+			table: { width: "100%", fontSize: "inherit"}
+		};
+		return (
+			<VBox style={{flex: 1, alignItems: "center"}}>
+				{ReactUtils.generateTable({
+					body: [
+						[
+							Weave.lang("File"),
+							this.props.fileInfo && this.props.fileInfo.fileName
+						],
+						[
+							Weave.lang("Size"),
+							this.props.fileInfo && this.props.fileInfo.fileSize
+						],
+						[
+							Weave.lang("Modified"),
+							this.props.fileInfo && this.props.fileInfo.lastModified
+						]
+					],
+					classes: {
+						td: [
+							"weave-left-cell",
+							"weave-right-cell"
+						]
+					},
+					styles: tableStyles
+				})}
+				<div style={{marginTop: 25}}>
+					{this.props.fileInfo && (thumbnail ? <img src={URL.createObjectURL(thumbnail)}/>:<div>{Weave.lang("Thumbnail Unavailable")}</div>)}
+
+				</div>
+			</VBox>
+		)
+	}
+}
