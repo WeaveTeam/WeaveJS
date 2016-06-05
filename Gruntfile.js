fe59b8dea2fca4b866fbd232e4613debeef09109
Sanjay Krishna Anbalagan <sanjay@iweave.com>
2016-06-05 11:43:12 -0400

    font section added to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -98,6 +98,7 @@ module.exports = function (grunt) {
         copy: {
             main: {expand: true, flatten: true, cwd: 'src/', src: '**/index.html', dest: 'dist/'},
             css: {expand: true, flatten: true, cwd: 'src/', src: 'css/*.css', dest: 'dist/'},
+            fonts: {expand: true, flatten: true, cwd: 'src/', src: 'css/fonts/*.ttf', dest: 'dist/fonts'},
             olcss: {expand: true, flatten: true, cwd: 'node_modules/openlayers/css', src: 'ol.css', dest: 'dist/'},
             fontawesomecss: {expand: true, flatten: true, cwd: 'node_modules/font-awesome/css', src: 'font-awesome.css', dest: 'dist/css/'},
             fontawesomefont: {expand: true, flatten: true, cwd: 'node_modules/font-awesome/fonts', src: '*', dest: 'dist/fonts/'},
