db371f628d1160f2b6ba8f7dc065cba8b2833799
Sanjay Krishna Anbalagan <sanjay@iweave.com>
2016-06-09 16:25:13 -0400

    Progress bar doesnt take extra space now and grey region wont be there after all task are done, instead it will overlay on nearby component, progress bar size can be adjusted in css now

--- srcts/react-ui/ProgressBar.tsx
+++ srcts/react-ui/ProgressBar.tsx
@@ -22,22 +22,27 @@ export default class ProgressBar extends React.Component<ProgressBarProps, Progr
 		super(props);
 	}
 
+	// important to keep its absolute and since its container is relative,
+	// it will position in the relative parent top left
+	// and since relative height is not given absolute values they will be zero as it doesn't contain any static children
+	// This will ensure progress bar won't take space on the screen and it overlays on the component below the progress bar
 	render()
 	{
+		let width:number =(this.props.progressValue || 0) / (this.props.total || 1) * 100;
+		let progressUI:JSX.Element = null;
+		// width gets value 0 when all task are done
+		if(width != 0 || this.props.visible)
+		{
+			// absolute child
+			progressUI =<div className="weave-progress-bar" style={ {width: String(width) + "%", position:"absolute"} }/>;
+		}
+		// relative Zero Height parent for absolute child
 		return (
-			<div
-				{...this.props as any}
+			<div {...this.props as any}
 				className={classnames("weave-progress-bar-background", this.props.className)}
-				style={_.merge({width: "100%", height: 2}, this.props.style)}
+				style={_.merge({width: "100%", position:"relative"}, this.props.style)}
 			>
-				<div
-					className="weave-progress-bar"
-					style={{
-						visibility: this.props.visible ? "visible" : "hidden",
-						width: ((this.props.progressValue || 0) / (this.props.total || 1) * 100) + "%",
-						height: "100%"
-					}}
-				/>
+				{progressUI}
 			</div>
 		);
 	}
