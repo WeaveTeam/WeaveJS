ff04b65028ced1e3a5e805164ff763c242fc08e8
Andy Dufilie <andrew.dufilie@iweave.com>
2016-06-24 12:20:30 -0400

    fixed incorrect usage of generateOpener()

--- srcts/ui/admin/ServiceLogin.tsx
+++ srcts/ui/admin/ServiceLogin.tsx
@@ -12,6 +12,8 @@ export default class ServiceLogin
 	service: WeaveAdminService;
 	context: React.ReactInstance;
 	login:Login;
+	onSuccess: (username: string) => void;
+	onCancel: () => void;
 
 	constructor(context: React.ReactInstance, service: WeaveAdminService)
 	{
@@ -21,29 +23,34 @@ export default class ServiceLogin
 
 	open=(onSuccess?: (username: string) => void, onCancel?: () => void)=>
 	{
-		this.generateOpener(onSuccess, onCancel)();
+		this.onSuccess = onSuccess;
+		this.onCancel = onCancel;
+		return this.show();
 	}
-
-	generateOpener(onSuccess?: (username: string) => void, onCancel?: () => void)
-	{
-		return PopupWindow.generateOpener(() => ({
-			context: this.context,
-			title: Weave.lang("Weave Server Sign-In"),
-			content: <Login ref={(c:Login) => this.login = c} onLogin={(fields: { username: string, password: string })=>this.handleLogin(fields, onSuccess)}/>,
-			footerContent: <div/>,
-			resizable: false,
-			draggable: false,
-			modal: true,
-			suspendEnter: true,
-			width: 400
-		}));
-	}
+	
+	private show = PopupWindow.generateOpener(() => ({
+		context: this.context,
+		title: Weave.lang("Weave Server Sign-In"),
+		content: (
+			<Login
+				ref={(c:Login) => this.login = c}
+				onLogin={ (fields: { username: string, password: string }) => this.handleLogin(fields, this.onSuccess) }
+			/>
+		),
+		footerContent: <div/>,
+		resizable: false,
+		draggable: false,
+		modal: true,
+		suspendEnter: true,
+		width: 400
+	}));
 
 	handleLogin = (fields: { username: string, password: string }, onSuccess: (username: string) => void): void => {
 		this.service.authenticate(fields.username, fields.password).then(
 			() => {
 				PopupWindow.close(this.login);
-				if (onSuccess) onSuccess(fields.username);
+				if (onSuccess)
+					onSuccess(fields.username);
 			},
 			(error: any) => {
 				this.login.invalid();
@@ -56,11 +63,13 @@ export default class ServiceLogin
 	{
 		this.service.getAuthenticatedUser().then(
 			(username: string) => {
-				if (username) {
+				if (username)
+				{
 					onSuccess && onSuccess(username);
 				}
-				else {
-					this.generateOpener(onSuccess, onCancel);
+				else
+				{
+					this.open(onSuccess, onCancel);
 				}
 			}
 		);
