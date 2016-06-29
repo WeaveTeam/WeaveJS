8ed846dbbee139b13628786aeff127eea8e47f20
Andy Dufilie <andrew.dufilie@iweave.com>
2016-06-29 12:10:23 -0400

    merged previous and new build.js script behaviors

--- WeaveASJS/build.js
+++ WeaveASJS/build.js
@@ -1,5 +1,9 @@
 const execSync = require('child_process').execSync;
-//process.env.FLEX_HOME="";
-var command = '"node_modules/flexjs/js/bin/mxmlc" -remove-circulars -js-compiler-option="--compilation_level WHITESPACE_ONLY" -fb "' + __dirname + '"';
+
+// prevent mxmlc from using FLEX_HOME environment variable
+process.env.FLEX_HOME="";
+
+var flexjsHome = process.env.FLEXJS_HOME || "node_modules/flexjs";
+var command = `"${flexjsHome}/js/bin/mxmlc" -remove-circulars -js-compiler-option="--compilation_level WHITESPACE_ONLY" -fb "${__dirname}"`;
 console.log(command);
 execSync(command, {stdio: "inherit"});
