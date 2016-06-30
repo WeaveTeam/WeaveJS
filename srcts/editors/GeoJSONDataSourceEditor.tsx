817b907bbc67d41a2d6e20502120da3de6d71bd3
shwetapurushe <purushe@iweave.com>
2016-06-30 15:11:36 -0400

    GeojsonDatasourceeditor now checks for a valid geo json object on file upload

--- srcts/editors/GeoJSONDataSourceEditor.tsx
+++ srcts/editors/GeoJSONDataSourceEditor.tsx
@@ -13,6 +13,7 @@ import ComboBox from "../semantic-ui/ComboBox";
 import HelpIcon from "../react-ui/HelpIcon";
 
 import GeoJSONDataSource = weavejs.data.source.GeoJSONDataSource;
+import GeoJSON = weavejs.geom.GeoJSON;
 import EntityNode = weavejs.data.hierarchy.EntityNode;
 import EntityType = weavejs.api.data.EntityType;
 import LinkableFile = weavejs.core.LinkableFile;
@@ -26,15 +27,18 @@ export default class GeoJSONDataSourceEditor extends DataSourceEditor
 	{
 		let dataSource = (this.props.dataSource as GeoJSONDataSource);
 		let propertyIds = dataSource.getPropertyNames().map((id:string)=>({label: id.toString(), value: id}));
-		let validExtension:boolean;
-		let acceptExtension:string = ".geojson,application/vnd.geo+json";
-		if (dataSource.url.value)
+		let validGeoJson:boolean = true;
+		let acceptExtension:string = ".json,.geojson,application/vnd.geo+json";
+		
+		//validate if the json is a geo json
+		if(dataSource.url.result)
 		{
-			let extension = dataSource.url.value.split('.').pop();
-			validExtension = _.includes(acceptExtension.split(','),"."+extension);
-		} else {
-			validExtension = true;
+			//var json = Weave.getDefinition("JSON");//TODO look into correct serializing of url result
+			var jsonObj = dataSource.url.result;
+			//var jsonObj = json.parse(jsonStr);
+			validGeoJson = GeoJSON.isGeoJSONObject(jsonObj);
 		}
+		
 		propertyIds.unshift({label:Weave.lang("Auto-generated keys"), value: null});
 
 		let editorFields:[React.ReactChild, React.ReactChild][] = [
@@ -42,16 +46,16 @@ export default class GeoJSONDataSourceEditor extends DataSourceEditor
 			[
 				<HBox className="weave-padded-hbox" style={{alignItems: "center", justifyContent: "flex-end"}}>
 					{Weave.lang("GeoJSON URL")}
-					<HelpIcon className={validExtension ? "":"fa-exclamation-triangle"} style={{color:validExtension? null:"#794B02"}}>
+					<HelpIcon className={validGeoJson ? "":"fa-exclamation-triangle"} style={{color:validGeoJson? null:"#794B02"}}>
 						<VBox>
-							{validExtension ? Weave.lang("The URL of the GeoJSON file to be used"):Weave.lang("Warning: The file you have chosen has an extension that does not match the expected extension.")}
+							{validGeoJson ? Weave.lang("The URL of the GeoJSON file to be used"):Weave.lang("Warning: The file you have chosen has an extension that does not match the expected extension.")}
 						</VBox>
 					</HelpIcon>
 				</HBox>,
 				<FileSelector style={{width: "100%"}}
-							  targetUrl={dataSource.url} 
-							  placeholder={Weave.lang("http://www.example.com/example.geojson")} 
-							  accept=".geojson,application/vnd.geo+json"/>
+							  targetUrl={dataSource.url}
+							  placeholder={Weave.lang("http://www.example.com/example.geojson")}
+							  accept={acceptExtension}/>
 			],
 			[
 				Weave.lang("Projection"),
