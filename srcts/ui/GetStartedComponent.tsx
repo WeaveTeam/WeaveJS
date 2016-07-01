5a0009ae7336c336f146e13d54c328f57946d5bf
Andy Dufilie <andrew.dufilie@iweave.com>
2016-06-30 22:26:27 -0400

    whitespace

--- srcts/ui/GetStartedComponent.tsx
+++ srcts/ui/GetStartedComponent.tsx
@@ -23,16 +23,24 @@ export default class GetStartedComponent extends React.Component<GetStartedCompo
 	{
 		super(props);
 		this.state = {
-			showInteractiveTourList:props.showInteractiveTourList != undefined ? props.showInteractiveTourList:false
-		}
+			showInteractiveTourList: (
+				props.showInteractiveTourList != undefined
+				?	props.showInteractiveTourList
+				:	false
+			)
+		};
 	}
 
 	componentWillReceiveProps(nextProps:GetStartedComponentProps)
 	{
-		if(this.props.showInteractiveTourList != nextProps.showInteractiveTourList)
+		if (this.props.showInteractiveTourList != nextProps.showInteractiveTourList)
 		{
 			this.setState({
-				showInteractiveTourList:nextProps.showInteractiveTourList != undefined ? nextProps.showInteractiveTourList:false
+				showInteractiveTourList: (
+					nextProps.showInteractiveTourList != undefined
+					?	nextProps.showInteractiveTourList
+					:	false
+				)
 			});
 		}
 	}
@@ -40,15 +48,15 @@ export default class GetStartedComponent extends React.Component<GetStartedCompo
 	enableInteractiveTourList=()=>
 	{
 		this.setState({
-			showInteractiveTourList:true
+			showInteractiveTourList: true
 		})
 	};
 
 	private items:any = {
-		csvToViz:{
-			steps:["CSV file", "Location","Preview","Charts" , "Charts menu","Tool editor"],
-			stepPointers:["CSV file", "Open file",null,"Charts","Charts menu",null],
-			contents:[
+		csvToViz: {
+			steps: ["CSV file", "Location", "Preview", "Charts", "Charts menu", "Tool editor"],
+			stepPointers: ["CSV file", "Open file", null, "Charts", "Charts menu", null],
+			contents: [
 				"Click on CSV file to load rows of data for visualization",
 				"Click on open a file button to load CSV file from your local machine",
 				"In this table you can see the data",
@@ -61,62 +69,65 @@ export default class GetStartedComponent extends React.Component<GetStartedCompo
 
 	interactiveListItemClick=(itemName:string)=>
 	{
-		InteractiveTour.startTour(this.items[itemName].steps,this.items[itemName].contents,this.items[itemName].stepPointers);
-		if(this.props.onViewSelect)
+		var item = this.items[itemName];
+		InteractiveTour.startTour(item.steps, item.contents, item.stepPointers);
+		if (this.props.onViewSelect)
 			this.props.onViewSelect("tour");
 	};
 
-	render() {
+	render()
+	{
 		let urlParams:any = MiscUtils.getUrlParams();
 
 		let containerStyle:React.CSSProperties = {
-			position:"absolute",
-			left:0,
-			top:0,
-			width:"100%",
-			height:"100%",
-			display:"flex",
-			flexDirection:"column",
+			position: "absolute",
+			left: 0,
+			top: 0,
+			width: "100%",
+			height: "100%",
+			display: "flex",
+			flexDirection: "column",
 			alignItems: "center",
 			justifyContent: "space-around"
 		};
 
 		let overlayStyle:React.CSSProperties = {
-			position:"absolute",
-			left:0,
-			top:0,
-			width:"100%",
-			height:"100%"
+			position: "absolute",
+			left: 0,
+			top: 0,
+			width: "100%",
+			height: "100%"
 		};
 
-
-		let interactiveTourListUI:JSX.Element  = null;
-		if(this.state.showInteractiveTourList)
+		let interactiveTourListUI:JSX.Element = null;
+		if (this.state.showInteractiveTourList)
 		{
-			interactiveTourListUI = <VBox style={ {position:"relative", fontFamily:"inherit"} } className="weave-guidance-list">
-										<span className="weave-guidance-list-item"
-										      onClick={ ()=>this.interactiveListItemClick("csvToViz") }>
-											1.  CSV Data to Visualization
-										</span>
-									</VBox>
+			interactiveTourListUI = (
+				<VBox style={ {position:"relative", fontFamily:"inherit"} } className="weave-guidance-list">
+					<span
+						className="weave-guidance-list-item"
+						onClick={ ()=>this.interactiveListItemClick("csvToViz") }
+					>
+						1. CSV Data to Visualization
+					</span>
+				</VBox>
+			);
 		}
 
-		let styleObj:React.CSSProperties = _.merge({},this.props.style,{position:"relative"});
-
+		let styleObj:React.CSSProperties = _.merge({}, this.props.style, {position: "relative"});
 
 		let itemTextStyle:React.CSSProperties = {
-			textAlign:"center"
+			textAlign: "center"
 		};
-
 
 		let disableStyle:React.CSSProperties = {
 			borderColor: "grey",
 			color: "grey",
-			textAlign:"center"
+			textAlign: "center"
 		};
 
 		// empty div required to move the getStarted item in center of the screen
-		let emptyDiv:JSX.Element = interactiveTourListUI? null : <div/>;
+		let emptyDiv:JSX.Element = interactiveTourListUI ? null : <div/>;
 
 		// important for logo to have position relative as the container's one of the child is absolute
 		/* both padded vbox and hbox class are added to getStarted container, as flex wrap is used , we can expect column layout when screen size is reduced, this ensures spacing*/
@@ -127,40 +138,45 @@ export default class GetStartedComponent extends React.Component<GetStartedCompo
 					<h1 style={ {position:"relative", whiteSpace: "nowrap"} } className="weave-getstarted-logo">
 						Weave <span style={ {color:"rgb(236, 131, 89)"} }>2</span>
 					</h1>
-					<HBox style={ {width:"100%",justifyContent: "space-around", position:"relative",flexWrap:"wrap"} } >
-						<VBox key="data"
-						      className="weave-getstarted-item"
-						      onClick={()=>this.props.onViewSelect("default") }>
+					<HBox style={ {width: "100%", justifyContent: "space-around", position: "relative", flexWrap: "wrap"} } >
+						<VBox
+							key="data"
+							className="weave-getstarted-item"
+							onClick={()=>this.props.onViewSelect("default") }
+						>
 							<div className="weave-getstarted-item-icon">
 								<i className="fa fa-database"></i>
 							</div>
-							<span style={itemTextStyle}> Load <span style={ {color:"rgb(236, 131, 89)"} }> Data</span></span>
+							<span style={itemTextStyle}> Load <span style={ {color: "rgb(236, 131, 89)"} }> Data</span></span>
 						</VBox>
-						<VBox key="charts"
-						      className="weave-getstarted-item"
-						      onClick={()=>this.props.onViewSelect("file")}>
+						<VBox
+							key="charts"
+							className="weave-getstarted-item"
+							onClick={()=>this.props.onViewSelect("file")}
+						>
 							<div className="weave-getstarted-item-icon">
 								<i className="fa fa-file"></i>
 							</div>
-							<span style={itemTextStyle}> Load <span style={ {color:"rgb(236, 131, 89)"} }> Session</span></span>
+							<span style={itemTextStyle}> Load <span style={ {color: "rgb(236, 131, 89)"} }> Session</span></span>
 						</VBox>
-						<VBox key="tutorials"
-						      className="weave-getstarted-item"
-						      style={ Boolean(urlParams.enableTour) ? {cursor:"pointer"} : {cursor:"default"} }
-						      onClick={Boolean(urlParams.enableTour) ? this.enableInteractiveTourList : null}
-						      >
+						<VBox
+							key="tutorials"
+							className="weave-getstarted-item"
+							style={ Boolean(urlParams.enableTour) ? {cursor: "pointer"} : {cursor: "default"} }
+							onClick={Boolean(urlParams.enableTour) ? this.enableInteractiveTourList : null}
+						>
 							<div style={ Boolean(urlParams.enableTour) ? {} : disableStyle } className="weave-getstarted-item-icon">
 								<i className="fa fa-book"></i>
 							</div>
 							<span style={ Boolean(urlParams.enableTour) ? itemTextStyle : disableStyle }>
-								Interactive <span style={ Boolean(urlParams.enableTour) ? {color:"rgb(236, 131, 89)"} : disableStyle }> Tour</span>
+								Interactive <span style={ Boolean(urlParams.enableTour) ? {color: "rgb(236, 131, 89)"} : disableStyle }> Tour</span>
 							</span>
 						</VBox>
 					</HBox>
 					{emptyDiv}
 					{interactiveTourListUI}
-
 				</div>
-			</div>);
+			</div>
+		);
 	}
 }
