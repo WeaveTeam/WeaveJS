c7e699069c4fbbbc521e239daa91194e08dd99e1
Philip Kovac <philip.kovac@iweave.com>
2016-07-11 13:28:04 -0400

    Added gulp-runtime-import-verify

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp        = require('gulp');
 var tscAutoref  = require('gulp-tsc-autoref');
+var runtimeImportVerify = require('./gulp-runtime-import-verify');
 
 gulp.task('default', function() {
 	gulp.src(['srcts/**/*.tsx', 'srcts/**/*.ts'])
 		.pipe(tscAutoref())
+                .pipe(runtimeImportVerify())
 		.pipe(gulp.dest('outtsref/'));
-
 });
