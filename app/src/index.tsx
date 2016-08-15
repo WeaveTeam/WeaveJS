f2b5faf11fdc45ce9dbf9fcea80d87d75aaca3cc
Philip Kovac <philip.kovac@iweave.com>
2016-08-15 15:01:29 -0400

    Don't embed WeaveApp directly from bundle.

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,7 +1,2 @@
 import * as weavejs from "weavejs";
-import * as jquery from "jquery";
-var $ = (jquery as any).default as typeof jquery;
-
-$(function () {
-	weavejs.util.EmbedUtils.embed({element: "weaveElt", mode: "splash"});
-});
+weavejs;
