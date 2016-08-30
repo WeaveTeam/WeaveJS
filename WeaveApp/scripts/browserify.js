64d475d95473424b4f23d592d183bdac6c8c7abf
Franck Kamayou <franckamayou@gmail.com>
2016-08-29 21:06:37 -0400

    browserify now compile libs separately

--- WeaveApp/scripts/browserify.js
+++ WeaveApp/scripts/browserify.js
@@ -1,7 +1,39 @@
 const fs = require("fs");
 const browserify = require("browserify");
-browserify({
-	entries: ["./bin/js/index.js"],
-	paths: ["./node_modules","./bin/js"],
-	debug: true
-}).bundle().pipe(fs.createWriteStream("./dist/weave-app.bundle.js"));
+var libs = [
+	"c3",
+	"clipboard",
+	"codemirror",
+	"d3",
+	"filesaver.js",
+	"fixed-data-table",
+	"jquery",
+	"lodash",
+	"openlayers",
+	"pixi.js",
+	"proj4",
+	"rc-slider",
+	"react",
+	"react-codemirror",
+	"react-color",
+	"react-dom",
+	"react-dropzone",
+	"react-notification-system",
+	"react-sparklines"
+];
+var mode = process.argv[2];
+if(mode == "libs")
+{
+	browserify({
+		paths: ["./node_modules"],
+		debug: false
+	}).require(libs).bundle().pipe(fs.createWriteStream("./dist/libs.js"));
+}
+else
+{
+	browserify({
+		entries: ["./bin/js/index.js"],
+		paths: ["./bin/js"],
+		debug: true
+	}).external(libs).bundle().pipe(fs.createWriteStream("./dist/weave-app.bundle.js"));
+}
